fc366cff7cfd089bd2920babd3e8bf3065d965f6
Ugur Cenar <cenar93@gmail.com>
2024-01-17 13:05:17 +0100

    Legg til openapi eksempel initaliserer

---
+++ bidrag-commons/src/main/kotlin/no/nav/bidrag/commons/util/OpenApiExamplesConfig.kt
@@ -0,0 +1,59 @@
+package no.nav.bidrag.commons.util
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.PathItem
+import io.swagger.v3.oas.models.examples.Example
+import org.springdoc.core.customizers.OpenApiCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
+
+@Configuration
+class OpenApiExamplesConfig {
+    @Bean
+    fun openApiCustomiser(examples: Collection<OpenApiExample>): OpenApiCustomizer {
+        return OpenApiCustomizer { openAPI ->
+            examples.forEach { example ->
+                openAPI.components.addExamples(example.name, example.example)
+                examples.groupBy { Pair(it.path, it.method) }.entries.forEach {
+                    openAPI.addExamplesToPath(it.key, it.value)
+                }
+            }
+        }
+    }
+
+    fun OpenAPI.addExamplesToPath(
+        operation: Pair<String, HttpMethod>,
+        openApiExamples: List<OpenApiExample>,
+    ) {
+        val requestBody =
+            paths[operation.first]?.getOperation(operation.second)
+                ?.requestBody
+
+        val jsonBody = requestBody?.content?.get(MediaType.APPLICATION_JSON_VALUE)
+
+        openApiExamples
+            .forEach {
+                jsonBody?.addExamples(it.name, it.example)
+            }
+    }
+
+    fun PathItem.getOperation(httpMethod: HttpMethod) =
+        when (httpMethod) {
+            HttpMethod.POST -> post
+            HttpMethod.PUT -> put
+            HttpMethod.PATCH -> patch
+            HttpMethod.DELETE -> delete
+            HttpMethod.OPTIONS -> options
+            HttpMethod.GET -> get
+            else -> null
+        }
+}
+
+data class OpenApiExample(
+    val example: Example,
+    val method: HttpMethod,
+    val path: String,
+    val name: String = example.description,
+)
