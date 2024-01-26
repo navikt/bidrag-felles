c5c584b89967631f9fe6240800751bc856e05f96
Ugur Cenar <cenar93@gmail.com>
2024-01-26 11:48:39 +0100

    Opprett grunnlagsobjekter for vedtak og beregning

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/grunnlag/BeregningInntektRapporteringPeriode.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/grunnlag/BeregningInntektRapporteringPeriode.kt
@@ -1,17 +1,6 @@
 package no.nav.bidrag.transport.behandling.beregning.felles.grunnlag
 
-import io.swagger.v3.oas.annotations.media.Schema
-import no.nav.bidrag.domene.enums.inntekt.Inntektsrapportering
-import no.nav.bidrag.domene.tid.ÅrMånedsperiode
-import java.math.BigDecimal
+import no.nav.bidrag.transport.behandling.felles.grunnlag.InntektsrapporteringPeriode
 
-@Schema(description = "Rapportert inntekt for person")
 @Deprecated("Bruk InntektsrapporteringPeriode", ReplaceWith("InntektsrapporteringPeriode"))
-data class BeregningInntektRapporteringPeriode(
-    @Schema(description = "Periode") val periode: ÅrMånedsperiode,
-    @Schema(description = "Type inntektsrapportering") val inntektsrapportering: Inntektsrapportering,
-    @Schema(description = "Referanse til barnet inntekten gjelder for") val gjelderBarn: String? = null,
-    @Schema(description = "Inntekt beløp") val beløp: BigDecimal,
-    @Schema(description = "Er registrert manuelt?") val manueltRegistrert: Boolean,
-    @Schema(description = "Skal være del av beregning?") val valgt: Boolean,
-)
+typealias BeregningInntektRapporteringPeriode = InntektsrapporteringPeriode
