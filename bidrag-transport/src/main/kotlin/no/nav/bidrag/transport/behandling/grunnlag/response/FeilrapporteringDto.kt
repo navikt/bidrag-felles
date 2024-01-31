04e24bcb60b66694d84da3b4bda4e90fbb9a88c8
LarsOttoHaugen <lars.otto.haugen@nav.no>
2024-01-31 14:47:13 +0100

    Justert litt på feilrapportering fra bidrag-grunnlag

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/response/FeilrapporteringDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/response/FeilrapporteringDto.kt
@@ -2,20 +2,15 @@ package no.nav.bidrag.transport.behandling.grunnlag.response
 
 import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domene.enums.grunnlag.GrunnlagRequestType
-import org.springframework.http.HttpStatusCode
+import no.nav.bidrag.domene.enums.grunnlag.HentGrunnlagFeiltype
 import java.time.LocalDate
 
 data class FeilrapporteringDto(
-    @Schema(description = "Type grunnlag")
     val grunnlagstype: GrunnlagRequestType,
     @Schema(description = "Id til personen grunnlaget er forsøkt hentet for")
     val personId: String?,
-    @Schema(description = "Periode fra-dato")
     val periodeFra: LocalDate?,
-    @Schema(description = "Periode til-dato")
     val periodeTil: LocalDate?,
-    @Schema(description = "Http statuskode")
-    val feilkode: HttpStatusCode?,
-    @Schema(description = "Feilmelding")
+    val feiltype: HentGrunnlagFeiltype,
     val feilmelding: String?,
 )
