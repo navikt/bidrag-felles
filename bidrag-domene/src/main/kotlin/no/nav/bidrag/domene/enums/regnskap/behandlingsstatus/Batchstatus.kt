5eb473a43afd82759af80323e37453d6ffc41e59
Simen Høston <simen.naper.hoston@webstep.no>
2024-02-14 13:14:19 +0100

    Oppdaterer batchstatus med nye verdier

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/regnskap/behandlingsstatus/Batchstatus.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/regnskap/behandlingsstatus/Batchstatus.kt
@@ -1,8 +1,12 @@
 package no.nav.bidrag.domene.enums.regnskap.behandlingsstatus
 
 enum class Batchstatus {
+    Received,
+    InProgress,
     Failed,
     Processing,
     Done,
     DoneWithWarnings,
+    Structuring,
+    Structured,
 }
