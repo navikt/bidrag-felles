a886fd158417d99806d8366e40a4d3c44a654189
LarsOttoHaugen <lars.otto.haugen@nav.no>
2024-02-26 13:02:55 +0100

    Byttet ut referanse med ident i beregningsobjekt for inntekt

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/Inntekt.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/Inntekt.kt
@@ -2,25 +2,25 @@ package no.nav.bidrag.transport.behandling.beregning.felles
 
 import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domene.enums.inntekt.Inntektsrapportering
+import no.nav.bidrag.domene.ident.Personident
 import no.nav.bidrag.domene.tid.ÅrMånedsperiode
 import no.nav.bidrag.transport.behandling.felles.grunnlag.DelberegningSumInntekt
-import no.nav.bidrag.transport.behandling.felles.grunnlag.Grunnlagsreferanse
 import java.math.BigDecimal
 
 @Schema(description = "Grunnlag for kategorisering, gruppering og beregning av inntekter")
 data class BeregnValgteInntekterGrunnlag(
     @Schema(description = "Beregningsperiode") val periode: ÅrMånedsperiode,
-    @Schema(description = "Referanse til barn som det skal beregnes for") val barnReferanseListe: List<Grunnlagsreferanse>,
-    @Schema(description = "Referanse til bidragsmottaker") val bidragsmottakerReferanse: Grunnlagsreferanse,
+    @Schema(description = "Liste over identer til barn som det skal beregnes for") val barnIdentListe: List<Personident>,
+    @Schema(description = "Ident til bidragsmottaker") val bidragsmottakerIdent: Personident,
     @Schema(description = "Periodisert liste over inntekter") val grunnlagListe: List<InntektsgrunnlagPeriode> = emptyList(),
 )
 
 data class InntektsgrunnlagPeriode(
     @Schema(description = "Perioden inntekten gjelder for") val periode: ÅrMånedsperiode,
     @Schema(description = "Type inntektsrapportering") val inntektsrapportering: Inntektsrapportering,
-    @Schema(description = "Referanse til barnet inntekten gjelder for") val gjelderBarn: Grunnlagsreferanse? = null,
+    @Schema(description = "Ident til barnet inntekten gjelder for") val inntektGjelderBarnIdent: Personident? = null,
     @Schema(description = "Inntekt beløp") val beløp: BigDecimal,
-    @Schema(description = "Referanse til den som eier inntekten") val gjelderReferanse: Grunnlagsreferanse,
+    @Schema(description = "Ident til den som eier inntekten") val inntektEiesAvIdent: Personident,
 )
 
 @Schema(description = "Resultatet av kategorisering, gruppering og beregning av inntekter")
@@ -30,6 +30,6 @@ data class BeregnValgteInntekterResultat(
 
 @Schema(description = "Periodisert inntekt per barn")
 data class InntektPerBarn(
-    @Schema(description = "Referanse til barn") val gjelderBarn: Grunnlagsreferanse? = null,
+    @Schema(description = "Referanse til barn") val inntektGjelderBarnIdent: Personident? = null,
     @Schema(description = "Liste over summerte inntektsperioder") var summertInntektListe: List<DelberegningSumInntekt> = emptyList(),
 )
