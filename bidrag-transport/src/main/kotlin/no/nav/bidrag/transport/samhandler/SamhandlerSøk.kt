e082e4202008d50e73f7b27e24b7258f8ea35039
simhos <104764765+simhos@users.noreply.github.com>
2024-04-02 11:12:44 +0200

    Oppretter nytt søkeobjekt for samhandlere (#97)

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/samhandler/SamhandlerSøk.kt
@@ -0,0 +1,16 @@
+package no.nav.bidrag.transport.samhandler
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Søkbare felter for opphenting av samhandlere.")
+data class SamhandlerSøk(
+    val navn: String? = null,
+    val postnummer: String? = null,
+    val poststed: String? = null,
+    val norskkontonr: String? = null,
+    val iban: String? = null,
+    val swift: String? = null,
+    val banknavn: String? = null,
+    val banklandkode: String? = null,
+    val bankcode: String? = null,
+)
