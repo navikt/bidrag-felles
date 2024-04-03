1f36457d3b057f89b778c64a1a428fe83c45daba
simhos <104764765+simhos@users.noreply.github.com>
2024-04-03 12:17:05 +0200

    Legger til felter for søk på ident og offentligId for samhandler (#98)

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/samhandler/SamhandlerSøk.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/samhandler/SamhandlerSøk.kt
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Søkbare felter for opphenting av samhandlere.")
 data class SamhandlerSøk(
+    val ident: String? = null,
+    val offentligId: String? = null,
     val navn: String? = null,
     val postnummer: String? = null,
     val poststed: String? = null,
