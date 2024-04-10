876bf7237c0e122de6d294cbda36323ad3f1ec9c
Simen Høston <simen.naper.hoston@webstep.no>
2024-04-10 11:10:11 +0200

    Legger til kontaktopplysninger og områdekode i samhandlerdto

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/samhandler/SamhandlerDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/samhandler/SamhandlerDto.kt
@@ -9,10 +9,26 @@ data class SamhandlerDto(
     val navn: String?,
     @Schema(description = "Offentlig id for samhandlere.")
     val offentligId: String? = null,
-    @Schema(description = "Type offentlig id. F.eks ORG for norske organisasjonsnummere.")
+    @Schema(
+        description =
+            "Type offentlig id. " +
+                "F.eks ORG for norske organisasjonsnummere, " +
+                "UTOR for utenlandske organisasjonsnummere, " +
+                "FNR for norske personnummer.",
+    )
     val offentligIdType: String? = null,
+    @Schema(description = "Definerer hvilket område samhandleren er knyttet til.")
+    val områdekode: Områdekode? = null,
     @Schema(description = "Samhandlerens adresse.")
     val adresse: AdresseDto? = null,
     @Schema(description = "Samhandlerens kontonummer. Kontonummer er ikke inkludert i søkeresultat mot TSS, kun i oppslag.")
     val kontonummer: KontonummerDto? = null,
+    @Schema(description = "Kontaktperson for samhandler. Benyttes primært av utland.")
+    val kontaktperson: String? = null,
+    @Schema(description = "Kontakt epost for samhandler. Benyttes primært av utland.")
+    val kontaktEpost: String? = null,
+    @Schema(description = "Kontakt telefon for samhandler. Benyttes primært av utland.")
+    val kontaktTelefon: String? = null,
+    @Schema(description = "Fritekstfelt for notater.")
+    val notat: String? = null,
 )
