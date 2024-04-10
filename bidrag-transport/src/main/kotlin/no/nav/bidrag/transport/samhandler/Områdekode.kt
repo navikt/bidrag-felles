7f36fe560321950fae40be5f843047a4b9aa94fa
simhos <104764765+simhos@users.noreply.github.com>
2024-04-10 11:13:43 +0200

    Legger til kontaktopplysninger og områdekode i samhandlerdto (#104)

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/samhandler/Områdekode.kt
@@ -0,0 +1,12 @@
+package no.nav.bidrag.transport.samhandler
+
+enum class Områdekode(val tssOmrådekode: String) {
+    UTENRIKSSTASJON("0050"),
+    ADVOKAT("0060"),
+    ARBEIDSGIVER("0070"),
+    REELL_MOTTAKER("0080"),
+    UTENLANDSK_PERSON("0260"),
+    UTENLANDSK_FOGD("2070"),
+    SOSIALKONTO("0280"),
+    BARNEVERNSINSTITUSJON("0290"),
+}
