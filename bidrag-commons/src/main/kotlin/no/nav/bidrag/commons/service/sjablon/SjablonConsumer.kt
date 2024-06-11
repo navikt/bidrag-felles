95fac0af1dc8a26d5b23992d83ee17ee41a51307
Magnus Rinnan <magnus.rinnan@nav.no>
2024-06-11 11:43:40 +0200

    Hent flere typer sjabloner SjablonConsumer bidrag-commons

--- bidrag-commons/src/main/kotlin/no/nav/bidrag/commons/service/sjablon/SjablonConsumer.kt
+++ bidrag-commons/src/main/kotlin/no/nav/bidrag/commons/service/sjablon/SjablonConsumer.kt
@@ -4,7 +4,10 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import no.nav.bidrag.commons.CorrelationId
 import no.nav.bidrag.commons.service.AppContext
 import no.nav.bidrag.commons.service.retryTemplate
+import no.nav.bidrag.commons.service.sjablon.SjablonProvider.Companion.SJABLONBIDRAGSEVNE_CACHE
+import no.nav.bidrag.commons.service.sjablon.SjablonProvider.Companion.SJABLONSAMVÆRSFRADRAG_CACHE
 import no.nav.bidrag.commons.service.sjablon.SjablonProvider.Companion.SJABLONTALL_CACHE
+import no.nav.bidrag.commons.service.sjablon.SjablonProvider.Companion.SJABLONTRINNVISSKATTESATS_CACHE
 import no.nav.bidrag.commons.util.secureLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
@@ -38,6 +41,10 @@ internal class SjablonConsumer(
     @Value("\${BIDRAG_SJABLON_URL}") url: String,
 ) {
     private val sjablontallUrl = "/sjablontall/all"
+    private val sjablonSamværsfradragUrl = "/samvaersfradrag/all"
+    private val sjablonBidragsevneUrl = "/bidragsevner/all"
+    private val sjablonTrinnvisSkattesatsUrl = "/trinnvisskattesats/all"
+
     val restTemplate: RestTemplate =
         RestTemplateBuilder()
             .rootUri(url)
@@ -65,11 +72,80 @@ internal class SjablonConsumer(
             throw exception
         }
     }
+
+    @Cacheable(SJABLONSAMVÆRSFRADRAG_CACHE)
+    fun hentSjablonSamværsfradrag(): ResponseEntity<List<Samværsfradrag>> {
+        try {
+            val sjablonResponse =
+                restTemplate.exchange(
+                    sjablonSamværsfradragUrl,
+                    HttpMethod.GET,
+                    null,
+                    object : ParameterizedTypeReference<List<Samværsfradrag>>() {},
+                )
+
+            logger.info { "Hentet sjabloner for samværsfradrag fra bidrag-sjablon" }
+            return sjablonResponse
+        } catch (exception: RestClientResponseException) {
+            logger.error(exception) {
+                "Det skjedde en feil ved henting av sjabloner for samværsfradrag med feil ${exception.statusText} og feilmelding " +
+                    "${exception.message}"
+            }
+            throw exception
+        }
+    }
+
+    @Cacheable(SJABLONBIDRAGSEVNE_CACHE)
+    fun hentSjablonBidragsevne(): ResponseEntity<List<Bidragsevne>> {
+        try {
+            val sjablonResponse =
+                restTemplate.exchange(
+                    sjablonBidragsevneUrl,
+                    HttpMethod.GET,
+                    null,
+                    object : ParameterizedTypeReference<List<Bidragsevne>>() {},
+                )
+
+            logger.info { "Hentet sjabloner for bidragsevne fra bidrag-sjablon" }
+            return sjablonResponse
+        } catch (exception: RestClientResponseException) {
+            logger.error(exception) {
+                "Det skjedde en feil ved henting av sjabloner for bidragsevne med feil ${exception.statusText} og feilmelding " +
+                    "${exception.message}"
+            }
+            throw exception
+        }
+    }
+
+    @Cacheable(SJABLONTRINNVISSKATTESATS_CACHE)
+    fun hentSjablonTrinnvisSkattesats(): ResponseEntity<List<TrinnvisSkattesats>> {
+        try {
+            val sjablonResponse =
+                restTemplate.exchange(
+                    sjablonTrinnvisSkattesatsUrl,
+                    HttpMethod.GET,
+                    null,
+                    object : ParameterizedTypeReference<List<TrinnvisSkattesats>>() {},
+                )
+
+            logger.info { "Hentet sjabloner for trinnvis skattesats fra bidrag-sjablon" }
+            return sjablonResponse
+        } catch (exception: RestClientResponseException) {
+            logger.error(exception) {
+                "Det skjedde en feil ved henting av sjabloner for trinnvis skattesats med feil ${exception.statusText} og feilmelding " +
+                    "${exception.message}"
+            }
+            throw exception
+        }
+    }
 }
 
 class SjablonProvider {
     companion object {
         const val SJABLONTALL_CACHE = "SJABLONTALL_CACHE"
+        const val SJABLONSAMVÆRSFRADRAG_CACHE = "SJABLONSAMVÆRSFRADRAG_CACHE"
+        const val SJABLONBIDRAGSEVNE_CACHE = "SJABLONBIDRAGSEVNE_CACHE"
+        const val SJABLONTRINNVISSKATTESATS_CACHE = "SJABLONTRINNVISSKATTESATS_CACHE"
 
         /**
          * Hent liste over alle sjablonverdier
@@ -83,6 +159,45 @@ class SjablonProvider {
                 }
             } catch (e: Exception) {
                 secureLogger.error(e) { "Det skjedde en feil ved henting av sjablontall" }
+                emptyList()
+            }
+        }
+
+        fun hentSjablonSamværsfradrag(): List<Samværsfradrag> {
+            return try {
+                retryTemplate(
+                    "SjablonProvider.hentSjablonSamværsfradrag",
+                ).execute<List<Samværsfradrag>, HttpClientErrorException> {
+                    AppContext.getBean("CommonsSjablonConsumer", SjablonConsumer::class.java).hentSjablonSamværsfradrag().body
+                }
+            } catch (e: Exception) {
+                secureLogger.error(e) { "Det skjedde en feil ved henting av sjablon for samværsfradrag" }
+                emptyList()
+            }
+        }
+
+        fun hentSjablonBidragsevne(): List<Bidragsevne> {
+            return try {
+                retryTemplate(
+                    "SjablonProvider.hentSjablonBidragsevne",
+                ).execute<List<Bidragsevne>, HttpClientErrorException> {
+                    AppContext.getBean("CommonsSjablonConsumer", SjablonConsumer::class.java).hentSjablonBidragsevne().body
+                }
+            } catch (e: Exception) {
+                secureLogger.error(e) { "Det skjedde en feil ved henting av sjablon for bidragsevne" }
+                emptyList()
+            }
+        }
+
+        fun hentSjablonTrinnvisSkattesats(): List<TrinnvisSkattesats> {
+            return try {
+                retryTemplate(
+                    "SjablonProvider.hentSjablonTrinnvisSkattesats",
+                ).execute<List<TrinnvisSkattesats>, HttpClientErrorException> {
+                    AppContext.getBean("CommonsSjablonConsumer", SjablonConsumer::class.java).hentSjablonTrinnvisSkattesats().body
+                }
+            } catch (e: Exception) {
+                secureLogger.error(e) { "Det skjedde en feil ved henting av sjablon for trinnvis skattesats" }
                 emptyList()
             }
         }
