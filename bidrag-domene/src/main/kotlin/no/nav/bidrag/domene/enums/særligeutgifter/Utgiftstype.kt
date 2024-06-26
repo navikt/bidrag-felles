c714b7f760e4e62fe1baca6a9e0ee565079fe8c5
Ugur Cenar <cenar93@gmail.com>
2024-06-26 07:52:48 +0200

    Særlige utgifter kategori

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/særligeutgifter/Utgiftstype.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/særligeutgifter/Utgiftstype.kt
@@ -1,14 +1,12 @@
 package no.nav.bidrag.domene.enums.særligeutgifter
 
-import no.nav.bidrag.domene.enums.vedtak.Engangsbeløptype
+enum class Utgiftstype(val kategori: SærligeutgifterKategori) {
+    KONFIRMASJONSAVGIFT(SærligeutgifterKategori.KONFIRMASJON),
+    KONFIRMASJONSLEIR(SærligeutgifterKategori.KONFIRMASJON),
+    SELSKAP(SærligeutgifterKategori.KONFIRMASJON),
+    KLÆR(SærligeutgifterKategori.KONFIRMASJON),
+    REISEUTGIFT(SærligeutgifterKategori.KONFIRMASJON),
 
-enum class Utgiftstype(val kategori: Engangsbeløptype) {
-    KONFIRMASJONSAVGIFT(Engangsbeløptype.SÆRTILSKUDD_KONFIRMASJON),
-    KONFIRMASJONSLEIR(Engangsbeløptype.SÆRTILSKUDD_KONFIRMASJON),
-    SELSKAP(Engangsbeløptype.SÆRTILSKUDD_KONFIRMASJON),
-    KLÆR(Engangsbeløptype.SÆRTILSKUDD_KONFIRMASJON),
-    REISEUTGIFT(Engangsbeløptype.SÆRTILSKUDD_KONFIRMASJON),
-
-    TANNREGULERING(Engangsbeløptype.SÆRTILSKUDD_TANNREGULERING),
-    OPTIKK(Engangsbeløptype.SÆRTILSKUDD_OPTIKK),
+    TANNREGULERING(SærligeutgifterKategori.TANNREGULERING),
+    OPTIKK(SærligeutgifterKategori.OPTIKK),
 }
