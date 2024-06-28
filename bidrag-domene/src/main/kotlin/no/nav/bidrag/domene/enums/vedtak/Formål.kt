61108f13e2919f44802f2a660656f374ee4f460f
Ugur Cenar <cenar93@gmail.com>
2024-06-28 10:00:33 +0200

    Nye felter og grunnlagsobjekter. Refaktorering SÆRBIDRAG

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/vedtak/Formål.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/vedtak/Formål.kt
@@ -3,5 +3,8 @@ package no.nav.bidrag.domene.enums.vedtak
 enum class Formål {
     FORSKUDD,
     BIDRAG,
+
+    @Deprecated("SÆRTILSKUDD er erstattet med SÆRBIDRAG", ReplaceWith("SÆRBIDRAG"))
     SÆRTILSKUDD,
+    SÆRBIDRAG,
 }
