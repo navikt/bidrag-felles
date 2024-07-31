ee2c3b68335d04cf19aa62d768df655c13c35169
Ugur Alpay Cenar <cenar93@gmail.com>
2024-07-31 14:06:01 +0200

    Grunnlag til objekt mapping støtte for bisys

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/felles/JsonUtils.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/felles/JsonUtils.kt
@@ -1,18 +1,9 @@
 package no.nav.bidrag.transport.felles
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.json.JsonMapper
-
-val commonObjectmapper =
-    JsonMapper
-        .builder()
-        .findAndAddModules()
-        .configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-        .configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        .build()
 
 // Bisys bruker eldre versjon av jackson og støtter derfor ikke JsonMapper
-val commonObjectmapperLegacy =
+val commonObjectmapper =
     ObjectMapper()
         .findAndRegisterModules()
         .configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
