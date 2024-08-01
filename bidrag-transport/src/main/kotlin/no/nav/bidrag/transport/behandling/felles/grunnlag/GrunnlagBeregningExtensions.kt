25c265a9c490667443d0de6fc501d6e28d74b7d8
Ugur Alpay Cenar <cenar93@gmail.com>
2024-08-01 14:14:40 +0200

    Nye grunnlagstyper for sjablon og hjelpefunksjoner for grunnlag beregning (#195)

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/GrunnlagBeregningExtensions.kt
@@ -0,0 +1,56 @@
+package no.nav.bidrag.transport.behandling.felles.grunnlag
+
+import no.nav.bidrag.domene.enums.grunnlag.Grunnlagstype
+import no.nav.bidrag.domene.enums.rolle.Rolletype
+import java.math.BigDecimal
+
+fun List<GrunnlagDto>.finnDelberegningBidragspliktigesAndelSærbidrag(
+    grunnlagsreferanseListe: List<Grunnlagsreferanse>,
+): DelberegningBidragspliktigesAndelSærbidrag? {
+    val sluttberegning = finnSluttberegningIReferanser(grunnlagsreferanseListe) ?: return null
+    val delberegningBidragspliktigesAndel =
+        find {
+            it.type == Grunnlagstype.DELBEREGNING_BIDRAGSPLIKTIGES_ANDEL_SÆRBIDRAG &&
+                sluttberegning.grunnlagsreferanseListe.contains(
+                    it.referanse,
+                )
+        } ?: return null
+    return delberegningBidragspliktigesAndel.innholdTilObjekt<DelberegningBidragspliktigesAndelSærbidrag>()
+}
+
+fun List<GrunnlagDto>.finnSluttberegningIReferanser(grunnlagsreferanseListe: List<Grunnlagsreferanse>) =
+    find {
+        listOf(
+            Grunnlagstype.SLUTTBEREGNING_FORSKUDD,
+            Grunnlagstype.SLUTTBEREGNING_SÆRBIDRAG,
+        ).contains(it.type) &&
+            grunnlagsreferanseListe.contains(it.referanse)
+    }
+
+fun List<GrunnlagDto>.finnTotalInntektForRolleEllerIdent(
+    grunnlagsreferanseListe: List<Grunnlagsreferanse>,
+    rolletype: Rolletype? = null,
+    personIdent: String? = null,
+): BigDecimal {
+    val sluttberegning =
+        finnSluttberegningIReferanser(grunnlagsreferanseListe)
+            ?: return BigDecimal.ZERO
+    val gjelderReferanse =
+        hentAllePersoner()
+            .find {
+                (rolletype == null || it.type == rolletype.tilGrunnlagstype()) &&
+                    (personIdent == null || it.personIdent == personIdent)
+            }?.referanse
+    val delberegningSumInntekter = finnGrunnlagSomErReferertAv(Grunnlagstype.DELBEREGNING_SUM_INNTEKT, sluttberegning)
+    val delberegningSumInntektForRolle =
+        if (gjelderReferanse.isNullOrEmpty()) {
+            delberegningSumInntekter.firstOrNull()
+        } else {
+            delberegningSumInntekter.find {
+                it.gjelderReferanse ==
+                    gjelderReferanse
+            }
+        }
+    return delberegningSumInntektForRolle?.innholdTilObjekt<DelberegningSumInntekt>()?.totalinntekt
+        ?: BigDecimal.ZERO
+}
