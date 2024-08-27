4a240fe427d7e534ff19ca66b00719fa1cd7cf0e
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2024-08-27 16:04:09 +0200

    Lagt til request og response for LøpendeBidragssaker i bidrag-stønad (#223)

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/stonad/request/LøpendeBidragssakerRequest.kt
@@ -0,0 +1,13 @@
+package no.nav.bidrag.transport.behandling.stonad.request
+
+import io.swagger.v3.oas.annotations.media.Schema
+import no.nav.bidrag.domene.ident.Personident
+import java.time.LocalDate
+
+@Schema(description = "Request for å hente alle løpende bidragssaker for angitt skyldner på angitt dato")
+data class LøpendeBidragssakerRequest(
+    @Schema(description = "Personen som er skyldner")
+    val skyldner: Personident,
+    @Schema(description = "Dato som det ønskes å hente gyldige perioder for")
+    val dato: LocalDate,
+)
