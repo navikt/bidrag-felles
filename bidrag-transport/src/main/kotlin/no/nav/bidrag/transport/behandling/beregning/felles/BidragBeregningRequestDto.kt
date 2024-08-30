0599f3f9bcaf4e3e26862fc0341661d57aa8ba97
Ugur Alpay Cenar <cenar93@gmail.com>
2024-08-30 11:50:23 +0200

    Legg til validering på BidragBeregningRequestDto

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/BidragBeregningRequestDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/BidragBeregningRequestDto.kt
@@ -1,5 +1,7 @@
 package no.nav.bidrag.transport.behandling.beregning.felles
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 import no.nav.bidrag.domene.enums.vedtak.Stønadstype
 import no.nav.bidrag.domene.ident.Personident
 import java.time.LocalDate
@@ -8,7 +10,11 @@ data class BidragBeregningRequestDto(
     val hentBeregningerFor: List<HentBidragBeregning>,
 ) {
     data class HentBidragBeregning(
+        @field:NotBlank(message = "Saksnummer kan ikke være blank")
+        @field:Size(max = 7, min = 7, message = "Saksnummer skal ha sju tegn")
         val saksnummer: String,
+        @field:NotBlank(message = "personidentBarn kan ikke være blank")
+        @field:Size(max = 11, message = "Personident kan ha maks 11 tegn")
         val personidentBarn: Personident,
         val datoSøknad: LocalDate,
         val stønadstype: Stønadstype,
