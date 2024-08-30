0e5265d7de33fb26fe71300360871b931ea03280
Ugur Alpay Cenar <cenar93@gmail.com>
2024-08-30 11:13:21 +0200

    Oppdater periode til gjelderFom på BidragBeregningResponsDto (#225)
    * Oppdater periode til gjelderFom på BidragBeregningResponsDto
    
    * Oppdater periode til gjelderFom på BidragBeregningResponsDto

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/BidragBeregningResponsDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/BidragBeregningResponsDto.kt
@@ -2,8 +2,8 @@ package no.nav.bidrag.transport.behandling.beregning.felles
 
 import no.nav.bidrag.domene.enums.beregning.Samværsklasse
 import no.nav.bidrag.domene.ident.Personident
-import no.nav.bidrag.domene.tid.Datoperiode
 import java.math.BigDecimal
+import java.time.LocalDate
 
 data class BidragBeregningResponsDto(
     val beregningListe: List<BidragBeregning>,
@@ -11,10 +11,10 @@ data class BidragBeregningResponsDto(
     data class BidragBeregning(
         val saksnummer: String,
         val personidentBarn: Personident,
-        val periode: Datoperiode,
+        val gjelderFom: LocalDate,
         val beregnetBeløp: BigDecimal,
         val faktiskBeløp: BigDecimal,
         val beløpSamvær: BigDecimal,
-        val samværsklasse: Samværsklasse,
+        val samværsklasse: Samværsklasse? = null,
     )
 }
