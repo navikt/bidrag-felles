362bcceb9f8e94256f845ff172cb1dd9fe4d5c44
Ugur Alpay Cenar <cenar93@gmail.com>
2024-10-03 15:15:48 +0200

    Legg til default verdi for tilleggsstønadBarnetilsynListe (#269)

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/response/HentGrunnlagDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/grunnlag/response/HentGrunnlagDto.kt
@@ -34,7 +34,7 @@ data class HentGrunnlagDto(
             "Indikator på om person mottar eller har mottatt tilleggsstønad til barnetilsyn i " +
                 "enten Arena eller tilleggsstonader-sak. Responsen skal utvides med periode og beløp senere",
     )
-    val tilleggsstønadBarnetilsynListe: List<TilleggsstønadGrunnlagDto>,
+    val tilleggsstønadBarnetilsynListe: List<TilleggsstønadGrunnlagDto> = emptyList(),
     @Schema(description = "Liste over evt. feil rapportert under henting av grunnlag")
     val feilrapporteringListe: List<FeilrapporteringDto>,
     val hentetTidspunkt: LocalDateTime,
