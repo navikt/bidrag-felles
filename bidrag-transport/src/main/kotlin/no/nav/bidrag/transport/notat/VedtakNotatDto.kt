e2da5074acbd886cbdcbc51edb5cb316df2b0b05
Ugur Alpay Cenar <cenar93@gmail.com>
2024-10-04 09:01:21 +0200

    Legg til erHistorisk felt i NotatInntektDto

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/notat/VedtakNotatDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/notat/VedtakNotatDto.kt
@@ -338,6 +338,7 @@ data class NotatInntektDto(
     val type: Inntektsrapportering,
     val medIBeregning: Boolean = false,
     val gjelderBarn: NotatRolleDto?,
+    val erHistorisk: Boolean = false,
     val inntektsposter: List<NotatInntektspostDto> = emptyList(),
 ) {
     val visningsnavn
