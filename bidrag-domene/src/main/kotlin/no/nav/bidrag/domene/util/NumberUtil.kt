80726d17e2b6e4f2c6ec71f0f671034b4f8096ac
Ugur Alpay Cenar <cenar93@gmail.com>
2024-10-24 12:49:44 +0200

    Kotlin utvidelser for avrunding av tall (BigDecimal) (#297)
    * Utvidelser for avrunding av BigDecimal
    
    * Utvidelser for avrunding av BigDecimal
    
    * Utvidelser for avrunding av BigDecimal
    
    * Utvidelser for avrunding av BigDecimal

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/util/NumberUtil.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/util/NumberUtil.kt
@@ -5,3 +5,10 @@ import java.math.MathContext
 import java.math.RoundingMode
 
 fun BigDecimal.årsbeløpTilMåndesbeløp() = divide(BigDecimal(12), MathContext(10, RoundingMode.HALF_UP))
+
+fun BigDecimal.avrundet(antallDesimaler: Int) = setScale(antallDesimaler, RoundingMode.HALF_UP)
+
+val BigDecimal.avrundetTilNærmesteTier get() = divide(BigDecimal.TEN, 0, RoundingMode.HALF_UP).multiply(BigDecimal.TEN)
+val BigDecimal.avrundetMedTiDesimaler get() = avrundet(10)
+val BigDecimal.avrundetMedToDesimaler get() = avrundet(2)
+val BigDecimal.avrundetMedNullDesimaler get() = avrundet(0)
