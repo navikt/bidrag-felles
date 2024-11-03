db04e7f9eda46c15e526690354186e7fe724f719
Ugur Alpay Cenar <cenar93@gmail.com>
2024-11-03 11:23:03 +0100

    Samværskalkulator validering

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/samvær/SamværskalkulatorDetaljer.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/samvær/SamværskalkulatorDetaljer.kt
@@ -1,16 +1,22 @@
 package no.nav.bidrag.transport.behandling.beregning.samvær
 
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.PositiveOrZero
 import no.nav.bidrag.domene.enums.samværskalkulator.SamværskalkulatorFerietype
 import no.nav.bidrag.domene.enums.samværskalkulator.SamværskalkulatorNetterFrekvens
 import java.math.BigDecimal
 
 data class SamværskalkulatorDetaljer(
     val ferier: List<SamværskalkulatorFerie> = emptyList(),
+    @field:PositiveOrZero(message = "Reglemessig samvær netter kan ikke være negativ")
+    @field:Max(value = 15, message = "Regelmessig samvær kan ikke være over 15 netter")
     val regelmessigSamværNetter: BigDecimal,
 ) {
     data class SamværskalkulatorFerie(
         val type: SamværskalkulatorFerietype,
+        @field:PositiveOrZero(message = "Bidragsmottaker netter kan ikke være negativ")
         val bidragsmottakerNetter: BigDecimal = BigDecimal.ZERO,
+        @field:PositiveOrZero(message = "Bidragspliktig netter kan ikke være negativ")
         val bidragspliktigNetter: BigDecimal = BigDecimal.ZERO,
         val frekvens: SamværskalkulatorNetterFrekvens,
     )
