052537c85fab3d343901ba7368681623f83d3acd
Magnus Rinnan <magnus.rinnan@nav.no>
2024-11-05 13:48:00 +0100

    Lagt til BarnetilsynMedStønadPeriode

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Underholdskostnad.kt
@@ -0,0 +1,24 @@
+package no.nav.bidrag.transport.behandling.felles.grunnlag
+
+import io.swagger.v3.oas.annotations.media.Schema
+import no.nav.bidrag.domene.enums.barnetilsyn.Skolealder
+import no.nav.bidrag.domene.enums.barnetilsyn.Tilsynstype
+import no.nav.bidrag.domene.tid.ÅrMånedsperiode
+import java.math.BigDecimal
+
+@Schema(description = "Underholdskostnad for person")
+data class UnderholdskostnadPeriode(
+    override val periode: ÅrMånedsperiode,
+    val beløp: BigDecimal,
+    override val manueltRegistrert: Boolean,
+) : GrunnlagPeriodeInnhold
+
+@Schema(description = "Barnetilsyn med stønad fra Enslig Forsørger")
+data class BarnetilsynMedStønadPeriode(
+    override val periode: ÅrMånedsperiode,
+    val gjelderBarn: Grunnlagsreferanse,
+    val beløp: Int?,
+    val tilsynstype: Tilsynstype?,
+    val skolealder: Skolealder?,
+    override val manueltRegistrert: Boolean,
+) : GrunnlagPeriodeInnhold
