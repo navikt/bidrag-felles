cac42cb4004f5ce183da81d54211b60fcf2d2b2b
Magnus Rinnan <magnus.rinnan@nav.no>
2024-11-18 14:36:24 +0100

    Delberegninger Barnetillegg

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Barnetillegg.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Barnetillegg.kt
@@ -1,8 +1,17 @@
 package no.nav.bidrag.transport.behandling.felles.grunnlag
 
+import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.bidrag.domene.enums.barnetillegg.Barnetilleggstype
 import no.nav.bidrag.domene.tid.ÅrMånedsperiode
 import java.math.BigDecimal
+
+@Schema(description = "Barnetillegg for person")
+data class BarnetilleggPeriode(
+    override val periode: ÅrMånedsperiode,
+    val beløp: BigDecimal,
+    val skattFaktor: BigDecimal,
+    override val manueltRegistrert: Boolean,
+) : GrunnlagPeriodeInnhold
 
 data class DelberegningBarnetilleggSkattesats(
     override val periode: ÅrMånedsperiode,
