8841d771bd76043bb5ccc28392bb4af5901698fc
Ugur Alpay Cenar <cenar93@gmail.com>
2024-11-28 07:47:14 +0100

    Nye grunnlagstyper og objekter for gebyrberegning

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Gebyr.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Gebyr.kt
@@ -1,5 +1,7 @@
 package no.nav.bidrag.transport.behandling.felles.grunnlag
 
+import no.nav.bidrag.domene.enums.grunnlag.Grunnlagstype
+import no.nav.bidrag.domene.enums.sjablon.SjablonTallNavn
 import java.math.BigDecimal
 
 data class ManueltOverstyrtGebyr(
@@ -15,3 +17,24 @@ data class DelberegningInnteksbasertGebyr(
 data class SluttberegningGebyr(
     val ilagtGebyr: Boolean,
 ) : GrunnlagInnhold
+
+val List<BaseGrunnlag>.gebyrDelberegningSumInntekt get() =
+    find {
+        it.type == Grunnlagstype.SLUTTBEREGNING_GEBYR
+    }?.let {
+        finnGrunnlagSomErReferertAv(Grunnlagstype.DELBEREGNING_SUM_INNTEKT, it)
+    }?.toList()
+        ?.innholdTilObjekt<DelberegningSumInntekt>()
+        ?.maxBy { it.barnetillegg ?: BigDecimal.ZERO }
+
+val List<BaseGrunnlag>.sluttberegningGebyr get() =
+    find {
+        it.type == Grunnlagstype.SLUTTBEREGNING_GEBYR
+    }?.innholdTilObjekt<SluttberegningGebyr>()
+
+val List<BaseGrunnlag>.gebyrBeløp get() =
+    filter {
+        it.type == Grunnlagstype.SJABLON_SJABLONTALL
+    }.innholdTilObjekt<SjablonSjablontallPeriode>()
+        .find { it.sjablon == SjablonTallNavn.FASTSETTELSESGEBYR_BELØP }
+        ?.verdi
