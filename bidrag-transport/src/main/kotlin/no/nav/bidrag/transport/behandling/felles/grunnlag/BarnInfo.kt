361c917816a2099eb4e28b240c8b9c00150eba1f
Simen Høston <simen.naper.hoston@webstep.no>
2025-01-06 14:18:22 +0100

    Renamer til barnInfo og gjør flere av feltene nullable

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/BarnInfo.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/BarnInfo.kt
@@ -3,11 +3,11 @@ package no.nav.bidrag.transport.behandling.felles.grunnlag
 import no.nav.bidrag.domene.ident.Personident
 import no.nav.bidrag.domene.tid.ÅrMånedsperiode
 
-data class Søknadsbarn(
+data class BarnInfo(
     val navn: String,
     val fødselsnummer: Personident,
     val periode: ÅrMånedsperiode,
-    val andelForsørget: Double,
-    val harSammeAdresse: Boolean,
-    val medIBeregning: Boolean
+    val andelForsørget: Double? = null,
+    val harSammeAdresse: Boolean? = null,
+    val medIBeregning: Boolean? = null
 ) : GrunnlagInnhold
