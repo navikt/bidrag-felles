e8d2096f8c4148bc1e91394bfc1a5226e25a65ee
Ugur Alpay Cenar <cenar93@gmail.com>
2025-01-21 16:31:42 +0100

    Endre antallBarnMedUtgifter til antallBarnMedTilsynsutgifter (#417)

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/notat/VedtakNotatDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/notat/VedtakNotatDto.kt
@@ -150,7 +150,7 @@ data class NotatUnderholdBarnDto(
         val sjablonMaksFradrag: BigDecimal,
         val antallBarnBMUnderTolvÅr: Int,
         val antallBarnBMBeregnet: Int = antallBarnBMUnderTolvÅr,
-        val antallBarnMedUtgifter: Int = antallBarnBMUnderTolvÅr,
+        val antallBarnMedTilsynsutgifter: Int = antallBarnBMUnderTolvÅr,
         val skattesatsFaktor: BigDecimal,
         val totalTilsynsutgift: BigDecimal,
         val sumTilsynsutgifter: BigDecimal,
