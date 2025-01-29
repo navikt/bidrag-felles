68cd5bfada865842193f2df3d6911e506c22d8f1
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2025-01-29 15:13:20 +0100

    Feature/stønad (#425)
    * Renamet grunnlagstype barnetilsyn med stønad
    
    * Nye transportobjekter for bidrag-stønad - begrenset revurdering

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/stonad/response/StønadMedPerioderResponse.kt
@@ -0,0 +1,22 @@
+package no.nav.bidrag.transport.behandling.stonad.response
+
+import io.swagger.v3.oas.annotations.media.Schema
+import no.nav.bidrag.domene.tid.ÅrMånedsperiode
+import java.math.BigDecimal
+
+@Schema(description = "Respons med stønad og beløpsinformasjon for alle perioder")
+data class StønadMedPerioderResponse(
+    @Schema(description = "Angir første år en stønad skal indeksreguleres")
+    val førsteIndeksreguleringsår: Int?,
+    @Schema(description = "Liste med perioder med beløpsinformasjon for stønaden")
+    val stønadsperiodeBeløpListe: List<StønadsperiodeBeløp> = emptyList(),
+)
+
+data class StønadsperiodeBeløp(
+    @Schema(description = "Periode med fra-og-med-dato og til-dato med format ÅÅÅÅ-MM")
+    val periode: ÅrMånedsperiode,
+    @Schema(description = "Beregnet stønadsbeløp")
+    val beløp: BigDecimal?,
+    @Schema(description = "Valutakoden tilhørende stønadsbeløpet")
+    val valutakode: String?,
+)
