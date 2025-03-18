9e7977a8942706af07b167072f431eb39295ab09
LarsOttoHaugen <lars.otto.haugen@nav.no>
2025-03-18 16:26:00 +0100

    Lagt til nye (kopi)objekter til bruk i aldersjustering

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Aldersjustering.kt
@@ -0,0 +1,32 @@
+package no.nav.bidrag.transport.behandling.felles.grunnlag
+
+import no.nav.bidrag.domene.enums.barnetilsyn.Skolealder
+import no.nav.bidrag.domene.enums.barnetilsyn.Tilsynstype
+import no.nav.bidrag.domene.enums.beregning.Samværsklasse
+import no.nav.bidrag.domene.tid.ÅrMånedsperiode
+import java.math.BigDecimal
+
+data class KopiDelberegningUnderholdskostnad(
+    override val periode: ÅrMånedsperiode,
+    override val fraVedtakId: Long,
+    val nettoTilsynsutgift: BigDecimal,
+) : GrunnlagPeriodeInnholdKopi
+
+data class KopiDelberegningBidragspliktigesAndel(
+    override val periode: ÅrMånedsperiode,
+    override val fraVedtakId: Long,
+    val endeligAndelFaktor: BigDecimal,
+) : GrunnlagPeriodeInnholdKopi
+
+data class KopiBarnetilsynMedStønadPeriode(
+    override val periode: ÅrMånedsperiode,
+    override val fraVedtakId: Long,
+    val tilsynstype: Tilsynstype,
+    val skolealder: Skolealder,
+) : GrunnlagPeriodeInnholdKopi
+
+data class KopiSamværsperiodeGrunnlag(
+    override val periode: ÅrMånedsperiode,
+    override val fraVedtakId: Long,
+    val samværsklasse: Samværsklasse,
+) : GrunnlagPeriodeInnholdKopi
