b782852f6e3ee057fecf57b091a4501a90348c62
Magnus Rinnan <magnus.rinnan@nav.no>
2025-03-24 16:33:32 +0100

    Flere klasser for vedtaksforslag

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/VedtaksforslagHendelse.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/VedtaksforslagHendelse.kt
@@ -1,21 +1,9 @@
 package no.nav.bidrag.transport.behandling.vedtak
 
-import no.nav.bidrag.domene.enums.vedtak.Beslutningstype
-import no.nav.bidrag.domene.enums.vedtak.Engangsbeløptype
-import no.nav.bidrag.domene.enums.vedtak.Innkrevingstype
-import no.nav.bidrag.domene.enums.vedtak.Stønadstype
 import no.nav.bidrag.domene.enums.vedtak.Vedtaksforslagsstatus
-import no.nav.bidrag.domene.enums.vedtak.Vedtakskilde
-import no.nav.bidrag.domene.enums.vedtak.Vedtakstype
-import no.nav.bidrag.domene.ident.Personident
-import no.nav.bidrag.domene.organisasjon.Enhetsnummer
 import no.nav.bidrag.domene.sak.Saksnummer
-import no.nav.bidrag.domene.tid.ÅrMånedsperiode
-import java.math.BigDecimal
-import java.time.LocalDate
-import java.time.LocalDateTime
 
-data class VedtakforslagHendelse(
+data class VedtaksforslagHendelse(
     val status: Vedtaksforslagsstatus,
     val id: Int,
     val saksnummerListe: List<Saksnummer>,
