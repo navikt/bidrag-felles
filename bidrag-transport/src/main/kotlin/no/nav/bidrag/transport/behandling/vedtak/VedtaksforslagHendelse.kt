7a5672aab2a8ff6c968245e61af481dee424b132
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2025-03-24 16:44:35 +0100

    Feature/ny hendelse vedtaksforslag (#500)
    * Flere klasser for vedtaksforslag
    
    * Flere klasser for vedtaksforslag
    
    * Flere klasser for vedtaksforslag

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/VedtaksforslagHendelse.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/VedtaksforslagHendelse.kt
@@ -1,22 +1,10 @@
 package no.nav.bidrag.transport.behandling.vedtak
 
-import no.nav.bidrag.domene.enums.vedtak.Beslutningstype
-import no.nav.bidrag.domene.enums.vedtak.Engangsbeløptype
-import no.nav.bidrag.domene.enums.vedtak.Innkrevingstype
-import no.nav.bidrag.domene.enums.vedtak.Stønadstype
-import no.nav.bidrag.domene.enums.vedtak.Vedtaksforslagsstatus
-import no.nav.bidrag.domene.enums.vedtak.Vedtakskilde
-import no.nav.bidrag.domene.enums.vedtak.Vedtakstype
-import no.nav.bidrag.domene.ident.Personident
-import no.nav.bidrag.domene.organisasjon.Enhetsnummer
+import no.nav.bidrag.domene.enums.vedtak.VedtaksforslagStatus
 import no.nav.bidrag.domene.sak.Saksnummer
-import no.nav.bidrag.domene.tid.ÅrMånedsperiode
-import java.math.BigDecimal
-import java.time.LocalDate
-import java.time.LocalDateTime
 
-data class VedtakforslagHendelse(
-    val status: Vedtaksforslagsstatus,
+data class VedtaksforslagHendelse(
+    val status: VedtaksforslagStatus,
     val id: Int,
     val saksnummerListe: List<Saksnummer>,
     val sporingsdata: Sporingsdata,
--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/response/VedtaksforslagDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/response/VedtaksforslagDto.kt
@@ -1,22 +1,10 @@
 package no.nav.bidrag.transport.behandling.vedtak.response
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.Min
-import jakarta.validation.constraints.NotBlank
-import no.nav.bidrag.domene.enums.vedtak.BehandlingsrefKilde
-import no.nav.bidrag.domene.enums.vedtak.Beslutningstype
-import no.nav.bidrag.domene.enums.vedtak.Engangsbeløptype
-import no.nav.bidrag.domene.enums.vedtak.Innkrevingstype
-import no.nav.bidrag.domene.enums.vedtak.Stønadstype
 import no.nav.bidrag.domene.enums.vedtak.Vedtakskilde
 import no.nav.bidrag.domene.enums.vedtak.Vedtakstype
-import no.nav.bidrag.domene.ident.Personident
 import no.nav.bidrag.domene.organisasjon.Enhetsnummer
-import no.nav.bidrag.domene.sak.Saksnummer
-import no.nav.bidrag.domene.tid.ÅrMånedsperiode
 import no.nav.bidrag.transport.behandling.felles.grunnlag.GrunnlagDto
-import no.nav.bidrag.transport.behandling.felles.grunnlag.Grunnlagsreferanse
-import java.math.BigDecimal
 import java.time.LocalDate
 import java.time.LocalDateTime
 
