d165e819697d4e4d2665340c1a4d3b3fdebdb235
Ugur Alpay Cenar <cenar93@gmail.com>
2025-03-28 06:53:01 +0100

    Årsakskoder tilpasse for bisys og lagt til stønadsid (#503)
    * Årsakskoder tilpasse for bisys og lagt til stønadsid
    
    * Årsakskoder tilpasse for bisys og lagt til stønadsid

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/sak/Stønadsid.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/sak/Stønadsid.kt
@@ -1,11 +1,23 @@
 package no.nav.bidrag.domene.sak
 
+import no.nav.bidrag.domene.enums.vedtak.Engangsbeløptype
 import no.nav.bidrag.domene.enums.vedtak.Stønadstype
 import no.nav.bidrag.domene.ident.Personident
 
 data class Stønadsid(
-    val gjelderBarn: Personident,
     val type: Stønadstype,
     val kravhaver: Personident,
+    val skyldner: Personident,
+    val sak: Saksnummer,
+) {
+    override fun toString(): String = "Stønadsid(type=$type, kravhaver=${kravhaver.verdi}, skyldner=${skyldner.verdi}, sak=${sak.verdi})"
+
+    fun toReferanse(): String = "stønadsid_type_${type}_kravhaver_${kravhaver.verdi}_skyldner_${skyldner.verdi}_sak_${sak.verdi}"
+}
+
+data class EngangsbeløpId(
+    val kravhaver: Personident,
+    val skyldner: Personident,
+    val type: Engangsbeløptype,
     val sak: Saksnummer,
 )
