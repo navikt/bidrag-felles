00e333673ab19bce9b256435eb4e82e64525849e
Ugur Alpay Cenar <cenar93@gmail.com>
2025-03-28 14:43:25 +0100

    Konstanter for aldersjustering

---
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/felles/BidragConstants.kt
@@ -0,0 +1,7 @@
+package no.nav.bidrag.domene.felles
+
+import no.nav.bidrag.domene.ident.Personident
+
+val enhet_farskap = "4860"
+val enhet_utland = "4865"
+val personidentNav = Personident("NAV")
