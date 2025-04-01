b4181d60c61974a0f49a24be6b46236fc0294217
Ugur Alpay Cenar <cenar93@gmail.com>
2025-04-01 14:56:52 +0200

    Privat avtale type enum

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/privatavtale/PrivatAvtaleType.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/privatavtale/PrivatAvtaleType.kt
@@ -1,5 +1,8 @@
 package no.nav.bidrag.domene.enums.privatavtale
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(enumAsRef = true)
 enum class PrivatAvtaleType {
     PRIVAT_AVTALE,
     DOM_RETTSFORLIK,
