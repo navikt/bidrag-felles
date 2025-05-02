dccb266d84b0300c1caeb550e2011fa18789310a
Lars Otto Haugen <45556722+LarsOttoHaugen@users.noreply.github.com>
2025-05-02 13:17:36 +0200

    Opprettet objekter for engangsbeløp til bruk i bidrag-stonad (#542)

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/stonad/request/HentStønadEngangsbeløpRequest.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/stonad/request/HentStønadEngangsbeløpRequest.kt
@@ -1,18 +1,20 @@
 package no.nav.bidrag.transport.behandling.stonad.request
 
 import io.swagger.v3.oas.annotations.media.Schema
-import no.nav.bidrag.domene.enums.vedtak.Stønadstype
+import no.nav.bidrag.domene.enums.vedtak.Engangsbeløptype
 import no.nav.bidrag.domene.ident.Personident
 import no.nav.bidrag.domene.sak.Saksnummer
 
-@Schema(description = "Request for å hente stønad som matcher angitte parametre")
-data class HentStønadRequest(
-    @Schema(description = "Stønadstype")
-    val type: Stønadstype,
+@Schema(description = "Request for å hente engangsbeløp som matcher angitte parametre")
+data class HentStønadEngangsbeløpRequest(
+    @Schema(description = "Engangsbeløptype")
+    val type: Engangsbeløptype,
     @Schema(description = "Referanse til sak")
     val sak: Saksnummer,
     @Schema(description = "Personidenten til den som skal betale stønaden")
     val skyldner: Personident,
     @Schema(description = "Personidenten til den som krever stønaden")
     val kravhaver: Personident,
+    @Schema(description = "Referanse")
+    val referanse: String,
 )
---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/stonad/response/StønadEngangsbeløpDto.kt
@@ -0,0 +1,62 @@
+package no.nav.bidrag.transport.behandling.stonad.response
+
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.persistence.Column
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import no.nav.bidrag.domene.enums.vedtak.Engangsbeløptype
+import no.nav.bidrag.domene.enums.vedtak.Innkrevingstype
+import no.nav.bidrag.domene.ident.Personident
+import no.nav.bidrag.domene.sak.Saksnummer
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+data class StønadEngangsbeløpDto(
+    @Schema(description = "Engangsbeløpsid")
+    val engangsbeløpsid: Int,
+    @Schema(description = "Engangsbeløptype")
+    val type: Engangsbeløptype,
+    @Schema(description = "Referanse til sak")
+    val sak: Saksnummer,
+    @Schema(description = "Personidenten til den som skal betale engangsbeløpet")
+    val skyldner: Personident,
+    @Schema(description = "Personidenten til den som krever engangsbeløpet")
+    val kravhaver: Personident,
+    @Schema(description = "Personidenten til den som mottar engangsbeløpet")
+    val mottaker: Personident,
+    @Schema(description = "Perioden er gyldig fra angitt tidspunkt (vedtakstidspunkt)")
+    val gyldigFra: LocalDateTime,
+    @Schema(description = "Angir tidspunkt perioden eventuelt er ugyldig fra (tidspunkt for vedtak med periode som erstattet denne)")
+    val gyldigTil: LocalDateTime?,
+    @Schema(description = "Periode-gjort-ugyldig-av-vedtaksid")
+    val periodeGjortUgyldigAvVedtaksid: Int?,
+    @Schema(description = "Beregnet engangsbeløp")
+    @Min(0)
+    val beløp: BigDecimal?,
+    @Schema(description = "Beløp BP allerede har betalt")
+    @Min(0)
+    val betaltBeløp: BigDecimal? = null,
+    @Schema(description = "Valutakoden tilhørende engangsbeløpet")
+    @NotBlank
+    val valutakode: String?,
+    @Schema(description = "Resultatkoden tilhørende engangsbeløpet")
+    @NotBlank
+    val resultatkode: String,
+    @Schema(description = "Angir om engangsbeløpet skal innkreves")
+    val innkreving: Innkrevingstype,
+    @Schema(
+        description =
+            "Referanse til engangsbeløp, brukes for å kunne omgjøre engangsbeløp senere i et klagevedtak. Unik innenfor et vedtak." +
+                "Referansen er enten angitt i requesten for opprettelse av vedtak " +
+                "eller generert av bidrag-vedtak hvis den ikke var angitt i requesten.",
+    )
+    val referanse: String,
+    @Column(nullable = false, name = "opprettet_av")
+    val opprettetAv: String = "",
+    @Column(nullable = false, name = "opprettet_tidspunkt")
+    val opprettetTidspunkt: LocalDateTime = LocalDateTime.now(),
+    @Column(nullable = true, name = "endret_av")
+    val endretAv: String? = null,
+    @Column(nullable = true, name = "endret_tidspunkt")
+    val endretTidspunkt: LocalDateTime? = null,
+)
