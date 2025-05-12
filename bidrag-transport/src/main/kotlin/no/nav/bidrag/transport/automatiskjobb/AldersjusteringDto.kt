ce60392d736e3b2b9feaaac1ec2adafc7bdf1b21
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-12 12:42:51 +0200

    Dto objekter for aldersjustering (#565)
    * Dto objekter for aldersjustering
    
    * Dto objekter for aldersjustering

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/automatiskjobb/AldersjusteringDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/automatiskjobb/AldersjusteringDto.kt
@@ -1,5 +1,6 @@
 package no.nav.bidrag.transport.automatiskjobb
 
+import no.nav.bidrag.domene.enums.vedtak.Stønadstype
 import no.nav.bidrag.domene.ident.Personident
 import no.nav.bidrag.domene.sak.Saksnummer
 import no.nav.bidrag.domene.sak.Stønadsid
@@ -16,18 +17,42 @@ data class AldersjusteringAldersjustertResultat(
     val vedtaksid: Int,
     val stønadsid: Stønadsid,
     val vedtak: OpprettVedtakRequestDto? = null,
-) : AldersjusteringResultat(true)
+) : AldersjusteringResultat(true) {
+    constructor() : this(
+        0,
+        Stønadsid(
+            Stønadstype.BIDRAG,
+            kravhaver = Personident(""),
+            skyldner = Personident(""),
+            sak = Saksnummer(""),
+        ),
+    )
+}
 
 data class AldersjusteringIkkeAldersjustertResultat(
     val stønadsid: Stønadsid,
     val begrunnelse: String,
     val aldersjusteresManuelt: Boolean = false,
-) : AldersjusteringResultat(false)
+) : AldersjusteringResultat(false) {
+    constructor() : this(
+        Stønadsid(
+            Stønadstype.BIDRAG,
+            kravhaver = Personident(""),
+            skyldner = Personident(""),
+            sak = Saksnummer(""),
+        ),
+        "",
+    )
+}
 
 abstract class AldersjusteringResultat(
     val aldersjustert: Boolean,
-)
+) {
+    constructor() : this(false)
+}
 
 data class AldersjusteringResultatlisteResponse(
     val resultatListe: List<AldersjusteringResultat>,
-)
+) {
+    constructor() : this(emptyList())
+}
