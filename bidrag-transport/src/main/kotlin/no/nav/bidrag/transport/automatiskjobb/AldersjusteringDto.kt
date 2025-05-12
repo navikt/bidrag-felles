fb16b4723ef1e6fdc0ef311f46eeec81fdd8b425
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-12 07:40:04 +0200

    Dto objekter for aldersjustering (#564)

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/automatiskjobb/AldersjusteringDto.kt
@@ -0,0 +1,33 @@
+package no.nav.bidrag.transport.automatiskjobb
+
+import no.nav.bidrag.domene.ident.Personident
+import no.nav.bidrag.domene.sak.Saksnummer
+import no.nav.bidrag.domene.sak.Stønadsid
+import no.nav.bidrag.transport.behandling.vedtak.request.OpprettVedtakRequestDto
+import java.time.Year
+
+data class HentAldersjusteringStatusRequest(
+    val saksnummer: Saksnummer,
+    val barnListe: List<Personident>,
+    val år: Int = Year.now().value,
+)
+
+data class AldersjusteringAldersjustertResultat(
+    val vedtaksid: Int,
+    val stønadsid: Stønadsid,
+    val vedtak: OpprettVedtakRequestDto? = null,
+) : AldersjusteringResultat(true)
+
+data class AldersjusteringIkkeAldersjustertResultat(
+    val stønadsid: Stønadsid,
+    val begrunnelse: String,
+    val aldersjusteresManuelt: Boolean = false,
+) : AldersjusteringResultat(false)
+
+abstract class AldersjusteringResultat(
+    val aldersjustert: Boolean,
+)
+
+data class AldersjusteringResultatlisteResponse(
+    val resultatListe: List<AldersjusteringResultat>,
+)
