1113d4a29c9847fd8ca8d192cfae476cf068a511
simhos <104764765+simhos@users.noreply.github.com>
2025-05-23 15:33:17 +0200

    Nye enum for Kreditortype, OffentligIdType, og Valutakode. Og utvider språk med visningsnavn og flere språk (#590)

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/diverse/Språk.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/diverse/Språk.kt
@@ -1,9 +1,45 @@
 package no.nav.bidrag.domene.enums.diverse
 
-enum class Språk {
-    NB,
-    NN,
-    DE,
-    FR,
-    EN,
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(enumAsRef = true, name = "Språk")
+enum class Språk(
+    val visningsnavn: String,
+) {
+    NB("Norsk bokmål"),
+    NN("Norsk nynorsk"),
+    AR("Arabisk"),
+    DA("Dansk"),
+    DE("Tysk"),
+    EN("Engelsk"),
+    EL("Gresk"),
+    ET("Estisk"),
+    ES("Spansk"),
+    FI("Finsk"),
+    FR("Franks"),
+    IS("Islandsk"),
+    IT("Italiensk"),
+    JA("Japansk"),
+    HR("Kroatisk"),
+    LV("Latvisk"),
+    LT("Litauisk"),
+    NL("Nederlandsk"),
+    PL("Polsk"),
+    PT("Portugisisk"),
+    RO("Rumensk"),
+    RU("Russisk"),
+    SR("Serbisk"),
+    SL("Slovensk"),
+    SK("Slovakisk"),
+    SV("Svensk"),
+    TH("Thai"),
+    TR("Tyrkisk"),
+    UK("Ukrainsk"),
+    HU("Ungarsk"),
+    VI("Vietnamesisk"),
+    ;
+
+    companion object {
+        fun fraVisningsnavn(visningsnavn: String): Språk? = entries.firstOrNull { it.visningsnavn == visningsnavn }
+    }
 }
