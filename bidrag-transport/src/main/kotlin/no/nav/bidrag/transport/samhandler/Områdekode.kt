a012f30e5f7a1d96241ca298c61cea633123021e
simhos <104764765+simhos@users.noreply.github.com>
2025-05-23 16:32:52 +0200

    Q1/samhandeler enums (#591)
    * Nye enum for Kreditortype, OffentligIdType, og Valutakode. Og utvider språk med visningsnavn og flere språk
    
    * Flytter funksjonaliteten for kreditortype til områdekode. Oppdaterer dto'er til å bruke enum

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/samhandler/Områdekode.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/samhandler/Områdekode.kt
@@ -2,18 +2,20 @@ package no.nav.bidrag.transport.samhandler
 
 enum class Områdekode(
     val tssOmrådekode: String,
-) {
-    UTENRIKSSTASJON("0050"),
-    ADVOKAT("0060"),
-    ARBEIDSGIVER("0070"),
-    REELL_MOTTAKER("0080"),
-    UTENLANDSK_PERSON("0260"),
-    UTENLANDSK_FOGD("0270"),
-    SOSIALKONTO("0280"),
-    BARNEVERNSINSTITUSJON("0290"),
+    val visningsnavn: String,
+    ) {
+    UTENRIKSSTASJON("0050", "Utenriksstasjon"),
+    ADVOKAT("0060", "Advokat"),
+    ARBEIDSGIVER("0070", "Arbeidsgiver"),
+    REELL_MOTTAKER("0080", "Reell mottaker"),
+    UTENLANDSK_PERSON("0260", "Utenlandsk person"),
+    UTENLANDSK_FOGD("0270", "Utenlandsk fogd"),
+    SOSIALKONTO("0280", "Sosialkontor"),
+    BARNEVERNSINSTITUSJON("0290", "Barnevernsinstitusjon"),
     ;
 
     companion object {
         fun fraTssOmrådekode(tssOmrådekode: String): Områdekode? = entries.find { it.tssOmrådekode == tssOmrådekode }
+        fun fraVisningsnavn(visningsnavn: String): Områdekode? = entries.find { it.visningsnavn == visningsnavn }
     }
 }
