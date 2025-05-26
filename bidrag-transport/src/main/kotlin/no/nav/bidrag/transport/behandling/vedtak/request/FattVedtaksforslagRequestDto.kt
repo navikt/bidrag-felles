a5cfbcdbad593b2e0900ee6336da18f2ad9dda6a
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2025-05-26 11:45:31 +0200

    Justeringer FattVedtaksforslagRequestDto (#595)

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/request/FattVedtaksforslagRequestDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/request/FattVedtaksforslagRequestDto.kt
@@ -7,12 +7,15 @@ import no.nav.bidrag.domene.sak.Saksnummer
 
 @Schema(description = "Request for å fatte vedtak for vedtaksforslag. Kan inneholde flere stønader innenfor et vedtaksforslag")
 data class FattVedtaksforslagRequestDto(
-    @Schema(description = "Vedtaksid for vedtaksforslag som det skal fattes vedtak for")
-    val vedtaksid: Long,
-    @Schema(description = "Saksnummer")
-    val sak: Saksnummer,
+    @Schema(description = "Liste over stønader som det skal fattes vedtak for")
+    val StønadListe: List<Stønad> = emptyList(),
+)
+
+data class Stønad(
     @Schema(description = "Hvilken type stønad det er snakk om")
     val type: Stønadstype,
+    @Schema(description = "Saksnummer")
+    val sak: Saksnummer,
     @Schema(description = "Personen som er skyldner i stønaden")
     val skyldner: Personident,
     @Schema(description = "Personen som er kravhaver i stønaden")
