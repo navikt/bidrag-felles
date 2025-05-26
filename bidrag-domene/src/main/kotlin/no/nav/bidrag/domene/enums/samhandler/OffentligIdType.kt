4820597039ede3df1705896634b1c1d5981a601f
simhos <104764765+simhos@users.noreply.github.com>
2025-05-26 15:13:06 +0200

    Legg til støtte for språk og valutaer visningskoder, samt oppdater OffentligIdType for å bruke enum uten visningsnavn for å heller ha det i egen fil (#596)

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/samhandler/OffentligIdType.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/samhandler/OffentligIdType.kt
@@ -3,16 +3,9 @@ package no.nav.bidrag.domene.enums.samhandler
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(enumAsRef = true, name = "Offentlig ID type")
-enum class OffentligIdType(
-    val visningsnavn: String,
-) {
-    NORSK_ORGNR("Norsk orgnummer"),
-    NORSK_FNR("Norsk personnummer"),
-    UTENLANDSK_ORGNR("Utenlandsk orgnummer"),
-    UTENLANDSK_FNR("Utenlandsk personnummer"),
-    ;
-
-    companion object {
-        fun fraVisningsnavn(visningsnavn: String): OffentligIdType? = entries.firstOrNull { it.visningsnavn == visningsnavn }
-    }
+enum class OffentligIdType {
+    NORSK_ORGNR,
+    NORSK_FNR,
+    UTENLANDSK_ORGNR,
+    UTENLANDSK_FNR,
 }
