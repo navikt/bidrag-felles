04caf6039b28fb4ccd8d4e2c0723ae99b0fd3b72
Ugur Alpay Cenar <cenar93@gmail.com>
2025-05-28 07:14:16 +0200

    VirkningstidspunktGrunnlag legg til opprinneligVirkningstidspunkt (#602)

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Søknad.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Søknad.kt
@@ -30,6 +30,7 @@ data class SøknadGrunnlag(
 
 data class VirkningstidspunktGrunnlag(
     val virkningstidspunkt: LocalDate,
+    val opprinneligVirkningstidspunkt: LocalDate? = null,
     val opphørsdato: LocalDate? = null,
     val årsak: VirkningstidspunktÅrsakstype? = null,
     val avslag: Resultatkode? = null,
