8fe00d1e0bd4ab211f91947df879bf9c5b44ce92
simhos <104764765+simhos@users.noreply.github.com>
2025-06-03 14:26:32 +0200

    Q1/sum pr ska (#605)
    * Legg til dataklasser for SumPrSakRequest og SumPrSakResponse
    
    * Fjern SumPrSakRequest dataklasse fra SumPrSak.kt

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/regnskap/avstemning/SumPrSak.kt
@@ -0,0 +1,17 @@
+package no.nav.bidrag.transport.regnskap.avstemning
+
+import io.swagger.v3.oas.annotations.media.Schema
+import java.math.BigDecimal
+
+data class SumPrSakResponse(
+    val saker: List<SumPrSak>,
+)
+
+data class SumPrSak(
+    @Schema(description = "Saksnummer.")
+    val saksnummer: String,
+    @Schema(description = "Antall innslag av innsendt stønadstype for saken.")
+    val antall: Long,
+    @Schema(description = "Samlet beløp for alle innslag i saken.")
+    val beløp: BigDecimal,
+)
