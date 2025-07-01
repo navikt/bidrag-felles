3ce781f053ee63703d2c6c126ef56b4a603abfad
Ugur Alpay Cenar <cenar93@gmail.com>
2025-07-01 08:38:46 +0200

    Forbedre meldingstekst for manuell aldersjustering i VedtakExtensions.kt (#641)

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/response/VedtakExtensions.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/response/VedtakExtensions.kt
@@ -117,7 +117,7 @@ fun tilAldersjusteringResultattekst(
             val stønadstype = if (stønadsendring.type == Stønadstype.FORSKUDD) "Forskuddet" else "Bidraget"
             return if (aldersjusteringDetaljerGrunnlag.innhold.aldersjustertManuelt) {
                 val barnInfo = person?.fødselsdato?.tilVisningsnavn()?.let { "født $it" } ?: stønadsendring.kravhaver.verdi
-                "$stønadstype til barn $barnInfo ble ikke aldersjustert etter manuell vurdering. Vedtaket grunnlaget hentes fra har resultat ${aldersjusteringDetaljerGrunnlag.innhold.førsteBegrunnelseVisningsnavn}"
+                "$stønadstype til barn $barnInfo ble ikke aldersjustert etter manuell vurdering. ${aldersjusteringDetaljerGrunnlag.innhold.førsteBegrunnelseVisningsnavn}"
             } else if (aldersjusteringDetaljerGrunnlag.innhold.aldersjusteresManuelt) {
                 val barnInfo = person?.fødselsdato?.tilVisningsnavn()?.let { "født $it" } ?: stønadsendring.kravhaver.verdi
                 "$stønadstype til barn $barnInfo skal aldersjusteres manuelt. ${aldersjusteringDetaljerGrunnlag.innhold.førsteBegrunnelseVisningsnavn}"
