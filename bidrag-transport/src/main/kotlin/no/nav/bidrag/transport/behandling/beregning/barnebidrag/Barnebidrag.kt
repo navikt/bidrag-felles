2ac9d9aa250dc236bb811f1fb77c5ec89f975241
Lars Otto Haugen <45556722+LarsOttoHaugen@users.noreply.github.com>
2025-07-10 07:50:00 +0200

    Nye objekter ifbm orkestrering av bidragsberegning. Oppdatert avhengigheter. (#649)

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/barnebidrag/Barnebidrag.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/barnebidrag/Barnebidrag.kt
@@ -1,9 +1,13 @@
 package no.nav.bidrag.transport.behandling.beregning.barnebidrag
 
 import io.swagger.v3.oas.annotations.media.Schema
+import no.nav.bidrag.domene.enums.beregning.Beregningstype
+import no.nav.bidrag.domene.sak.Stønadsid
 import no.nav.bidrag.domene.tid.ÅrMånedsperiode
+import no.nav.bidrag.transport.behandling.beregning.felles.BeregnGrunnlag
 import no.nav.bidrag.transport.behandling.felles.grunnlag.GrunnlagDto
 import java.math.BigDecimal
+import java.util.Collections.emptyList
 
 @Schema(description = "Resultatet av en barnebidragsberegning")
 data class BeregnetBarnebidragResultat(
@@ -23,4 +27,42 @@ data class ResultatPeriode(
 @Schema(description = "Resultatet av en beregning")
 data class ResultatBeregning(
     @Schema(description = "Resultat beløp") var beløp: BigDecimal?,
+)
+
+@Schema(description = "Request til BidragsberegningOrkestrator")
+data class BidragsberegningOrkestratorRequest(
+    @Schema(description = "Grunnlag for beregning av barnebidrag")
+    val beregnGrunnlag: BeregnGrunnlag? = null,
+    @Schema(description = "Grunnlag for orkestrering av klage")
+    val klageOrkestratorGrunnlag: KlageOrkestratorGrunnlag? = null,
+    @Schema(description = "Type beregning")
+    val beregningstype: Beregningstype = Beregningstype.BIDRAG,
+)
+
+@Schema(description = "Grunnlag for orkestrering av aldersjustering")
+data class KlageOrkestratorGrunnlag(
+    @Schema(description = "Id til stønad")
+    val stønad: Stønadsid,
+    @Schema(description = "Resultat av klageberegning")
+    val klageberegningResultat: BeregnetBarnebidragResultat,
+    @Schema(description = "Perioden klagen gjelder for")
+    val klageperiode: ÅrMånedsperiode,
+    @Schema(description = "Vedtaksid til påklaget vedtak")
+    val påklagetVedtakId: Int,
+)
+
+@Schema(description = "Response fra BidragsberegningOrkestrator")
+data class BidragsberegningOrkestratorResponse(
+    @Schema(description = "Liste over resultat av bidragsberegning")
+    val resultatVedtakListe: List<ResultatVedtak> = emptyList(),
+)
+
+@Schema(description = "Resultat av bidragsberegning")
+data class ResultatVedtak(
+    @Schema(description = "Resultat av beregning")
+    val resultat: BeregnetBarnebidragResultat,
+    @Schema(description = "Er delvedtak?")
+    val delvedtak: Boolean = false,
+    @Schema(description = "Er klagevedtak?")
+    val klagevedtak: Boolean = false,
 )
