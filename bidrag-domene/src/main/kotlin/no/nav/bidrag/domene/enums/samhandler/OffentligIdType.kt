20c40f39ee3b311128b1cb258f446ca9c09a132e
simhos <104764765+simhos@users.noreply.github.com>
2025-07-21 10:44:23 +0200

    Offentlig id type har nå beskrivelser (#664)
    * Offentlig id type har nå beskrivelser
    
    * Bump spring-core version to 6.2.9
    
    * Bump mockk version to 1.14.5
    
    * Bump jackson version to 2.19.2
    
    * Bump spring-expression version to 6.2.9
    
    * Update ktlint version to 1.7.0
    
    * Ktlint

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/samhandler/OffentligIdType.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/samhandler/OffentligIdType.kt
@@ -3,11 +3,13 @@ package no.nav.bidrag.domene.enums.samhandler
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(enumAsRef = true, name = "Offentlig ID type")
-enum class OffentligIdType {
-    ORG,
-    FNR,
-    UTOR,
-    UTPE,
-    DNR,
-    HPR,
+enum class OffentligIdType(
+    beskrivelse: String,
+) {
+    ORG("Organisasjonsnummer"),
+    FNR("Norsk fødselsnummer"),
+    UTOR("Utenlandsk organisasjonsnummer"),
+    UTPE("Utenlandsk personnummer"),
+    DNR("D-nummer"),
+    HPR("Helsepersonellregisteret"),
 }
