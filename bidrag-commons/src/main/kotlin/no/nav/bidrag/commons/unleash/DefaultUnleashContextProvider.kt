cf8be5f6c8e9ed2918d02b7e5fb0226f507129cf
Ugur Alpay Cenar <cenar93@gmail.com>
2025-07-22 15:14:56 +0200

    Unleash legg til saksnummer (#667)

--- bidrag-commons/src/main/kotlin/no/nav/bidrag/commons/unleash/DefaultUnleashContextProvider.kt
+++ bidrag-commons/src/main/kotlin/no/nav/bidrag/commons/unleash/DefaultUnleashContextProvider.kt
@@ -2,15 +2,24 @@ package no.nav.bidrag.commons.unleash
 
 import io.getunleash.UnleashContext
 import io.getunleash.UnleashContextProvider
+import no.nav.bidrag.commons.web.MdcConstants
 import org.slf4j.MDC
 
 class DefaultUnleashContextProvider : UnleashContextProvider {
     override fun getContext(): UnleashContext {
         val userId = MDC.get("user")
-        return UnleashContext
-            .builder()
-            .userId(userId)
-            .appName(MDC.get("applicationKey"))
-            .build()
+        val context =
+            UnleashContext
+                .builder()
+                .userId(userId)
+                .appName(MDC.get("applicationKey"))
+
+        MDC.get(MdcConstants.MDC_ENHET)?.let {
+            context.addProperty("enhet", it)
+        }
+        MDC.get(MdcConstants.MDC_SAKSNUMMER)?.let {
+            context.addProperty("saksnummer", it)
+        }
+        return context.build()
     }
 }
