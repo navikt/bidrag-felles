b094eb2ee516b3433738da31cf6ee1677e40b045
Ugur Alpay Cenar <cenar93@gmail.com>
2025-07-31 13:49:07 +0200

    Klage informasjon om gjennopprettet beløpshistorikk (#679)

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/ResultatFraVedtakGrunnlag.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/ResultatFraVedtakGrunnlag.kt
@@ -2,4 +2,6 @@ package no.nav.bidrag.transport.behandling.felles.grunnlag
 
 data class ResultatFraVedtakGrunnlag(
     val vedtaksid: Int,
+    val klagevedtak: Boolean = false,
+    val gjenopprettetBeløpshistorikk: Boolean = false,
 ) : GrunnlagInnhold
