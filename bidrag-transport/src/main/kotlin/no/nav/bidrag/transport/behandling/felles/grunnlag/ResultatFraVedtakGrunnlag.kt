c39a543471e008cde42be7b160b2f17475e8d87f
Ugur Alpay Cenar <cenar93@gmail.com>
2025-08-04 07:59:13 +0200

    Vedtaksid nullable i ResultatFraVedtakGrunnlag

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/ResultatFraVedtakGrunnlag.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/ResultatFraVedtakGrunnlag.kt
@@ -1,7 +1,7 @@
 package no.nav.bidrag.transport.behandling.felles.grunnlag
 
 data class ResultatFraVedtakGrunnlag(
-    val vedtaksid: Int,
+    val vedtaksid: Int?,
     val klagevedtak: Boolean = false,
     val gjenopprettetBeløpshistorikk: Boolean = false,
 ) : GrunnlagInnhold
