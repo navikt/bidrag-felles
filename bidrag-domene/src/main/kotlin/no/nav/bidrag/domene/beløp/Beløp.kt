d98fed68220bf2d189aff5cf92f2daff7e854447
Ugur Alpay Cenar <cenar93@gmail.com>
2025-08-08 12:46:07 +0200

    Beløp til en data class

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/beløp/Beløp.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/beløp/Beløp.kt
@@ -3,14 +3,13 @@
 package no.nav.bidrag.domene.beløp
 
 import no.nav.bidrag.domene.enums.samhandler.Valutakode
-import no.nav.bidrag.domene.felles.Verdiobjekt
 import java.math.BigDecimal
 
 data class Beløp(
-    override val verdi: BigDecimal,
+    val verdi: BigDecimal,
     val valutakode: Valutakode? = Valutakode.NOK,
-) : Verdiobjekt<BigDecimal>() {
-    override fun gyldig(): Boolean = verdi >= BigDecimal.ZERO && (valutakode == null || valutakode in Valutakode.entries)
+) {
+    constructor(verdi: Int) : this(verdi = verdi.toBigDecimal())
 
     fun erINorskeKroner() = valutakode == null || valutakode == Valutakode.NOK
 
