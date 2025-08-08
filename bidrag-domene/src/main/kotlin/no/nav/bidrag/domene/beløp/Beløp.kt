231bafdf9280013037b7993c42dcfe9a100f64ff
Ugur Alpay Cenar <cenar93@gmail.com>
2025-08-08 12:07:55 +0200

    Endre Beløp til data class

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/beløp/Beløp.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/beløp/Beløp.kt
@@ -4,12 +4,9 @@ package no.nav.bidrag.domene.beløp
 
 import no.nav.bidrag.domene.enums.samhandler.Valutakode
 import no.nav.bidrag.domene.felles.Verdiobjekt
-import no.nav.bidrag.domene.ident.Organisasjonsnummer
-import no.nav.bidrag.domene.ident.Personident
-import no.nav.bidrag.domene.ident.SamhandlerId
 import java.math.BigDecimal
 
-class Beløp(
+data class Beløp(
     override val verdi: BigDecimal,
     val valutakode: Valutakode? = Valutakode.NOK,
 ) : Verdiobjekt<BigDecimal>() {
