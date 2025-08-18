64a0795791c0d46c076d8e73e3738e1a5205eb7f
simhos <104764765+simhos@users.noreply.github.com>
2025-08-18 11:56:39 +0200

    Q1/tilgangskontroll (#694)
    * Endrer tilgangsklienten til å kalle nye endepunktene i tilgangskontroll
    
    * Nye transportobjekter for tilgangskontroll

--- bidrag-commons/src/main/kotlin/no/nav/bidrag/commons/tilgang/TilgangClient.kt
+++ bidrag-commons/src/main/kotlin/no/nav/bidrag/commons/tilgang/TilgangClient.kt
@@ -3,6 +3,7 @@ package no.nav.bidrag.commons.tilgang
 import no.nav.bidrag.commons.web.client.AbstractRestClient
 import no.nav.bidrag.commons.web.config.RestOperationsAzure
 import no.nav.bidrag.transport.tilgang.Sporingsdata
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Import
@@ -19,52 +20,74 @@ class TilgangClient(
     @Value("\${BIDRAG_TILGANG_URL}") private val tilgangURI: URI,
     @Qualifier("azure") private val restTemplate: RestTemplate,
 ) : AbstractRestClient(restTemplate, "tilgang") {
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     val sakUri =
         UriComponentsBuilder
             .fromUri(tilgangURI)
-            .pathSegment("api", "tilgang", "sak")
+            .pathSegment("v2", "api", "tilgang", "sak")
             .build()
             .toUri()
     val personUri =
         UriComponentsBuilder
             .fromUri(tilgangURI)
-            .pathSegment("api", "tilgang", "person")
+            .pathSegment("v2", "api", "tilgang", "person")
             .build()
             .toUri()
     val sporingsdataSakUri =
         UriComponentsBuilder
             .fromUri(tilgangURI)
-            .pathSegment("api", "sporingsdata", "sak")
+            .pathSegment("v2", "api", "sporingsdata", "sak")
             .build()
             .toUri()
     val sporingsdataPersonUri =
         UriComponentsBuilder
             .fromUri(tilgangURI)
-            .pathSegment("api", "sporingsdata", "person")
+            .pathSegment("v2", "api", "sporingsdata", "person")
             .build()
             .toUri()
 
     fun harTilgangSaksnummer(saksnummer: String): Boolean {
-        val headers = HttpHeaders()
-        headers.contentType = MediaType.TEXT_PLAIN
-        return postForNonNullEntity(sakUri, saksnummer, headers)
+        try {
+            val headers = HttpHeaders()
+            headers.contentType = MediaType.TEXT_PLAIN
+            return postForNonNullEntity(sakUri, saksnummer, headers)
+        } catch (e: Exception) {
+            logger.error("Feil ved sjekk på tilgang til saksnummer $saksnummer ", e)
+            return false
+        }
     }
 
     fun harTilgangPerson(personIdent: String): Boolean {
-        val headers = HttpHeaders()
-        headers.contentType = MediaType.TEXT_PLAIN
-        return postForNonNullEntity(personUri, personIdent, headers)
+        try {
+            val headers = HttpHeaders()
+            headers.contentType = MediaType.TEXT_PLAIN
+            return postForNonNullEntity(personUri, personIdent, headers)
+        } catch (e: Exception) {
+            logger.error("Feil ved sjekk på tilgang til person ", e)
+            return false
+        }
     }
 
     fun hentSporingsdataSak(saksnummer: String): Sporingsdata {
-        val headers = HttpHeaders()
-        headers.contentType = MediaType.TEXT_PLAIN
-        return postForNonNullEntity(sporingsdataSakUri, saksnummer, headers)
+        try {
+            val headers = HttpHeaders()
+            headers.contentType = MediaType.TEXT_PLAIN
+            return postForNonNullEntity(sporingsdataSakUri, saksnummer, headers)
+        } catch (e: Exception) {
+            logger.error("Feil ved henting av sporingsdata for sak $saksnummer ", e)
+            throw e
+        }
     }
 
     fun hentSporingsdataPerson(personIdent: String): Sporingsdata {
-        val headers = HttpHeaders()
-        headers.contentType = MediaType.TEXT_PLAIN
-        return postForNonNullEntity(sporingsdataPersonUri, personIdent, headers)
+        try {
+            val headers = HttpHeaders()
+            headers.contentType = MediaType.TEXT_PLAIN
+            return postForNonNullEntity(sporingsdataPersonUri, personIdent, headers)
+        } catch (e: Exception) {
+            logger.error("Feil ved henting av sporingsdata for person ", e)
+            throw e
+        }
     }
 }
