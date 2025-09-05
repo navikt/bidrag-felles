de53d933382087c2a7156a663bc33cccc7cd6bd0
Ugur Alpay Cenar <cenar93@gmail.com>
2025-09-05 12:36:43 +0200

    Legg til OMGJORT_VEDTAK_VEDTAKSTIDSPUNKT i BeregnTil

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/vedtak/BeregnTil.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/vedtak/BeregnTil.kt
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(enumAsRef = true)
 enum class BeregnTil {
     OPPRINNELIG_VEDTAKSTIDSPUNKT,
+
+    // Hvis det er opprettet klage på klage så vil OPPRINNELIG_VEDTAKSTIDSPUNKT og OMGJORT_VEDTAK_VEDTAKSTIDSPUNKT være ulik. Ellers er det samme
+    OMGJORT_VEDTAK_VEDTAKSTIDSPUNKT,
     INNEVÆRENDE_MÅNED,
     ETTERFØLGENDE_MANUELL_VEDTAK,
 }
