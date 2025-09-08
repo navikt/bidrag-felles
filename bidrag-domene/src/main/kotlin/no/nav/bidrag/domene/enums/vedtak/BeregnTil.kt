fbb11cdf7def4515f17029ffcebcb7b425910f3f
Ugur Alpay Cenar <cenar93@gmail.com>
2025-09-08 10:48:56 +0200

    Fjerne OMGJORT_VEDTAK_VEDTAKSTIDSPUNKT fra BeregnTil

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/vedtak/BeregnTil.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/vedtak/BeregnTil.kt
@@ -1,13 +1,13 @@
 package no.nav.bidrag.domene.enums.vedtak
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(enumAsRef = true)
 enum class BeregnTil {
+    @JsonAlias("OMGJORT_VEDTAK_VEDTAKSTIDSPUNKT")
     OPPRINNELIG_VEDTAKSTIDSPUNKT,
 
-    // Hvis det er opprettet klage på klage så vil OPPRINNELIG_VEDTAKSTIDSPUNKT og OMGJORT_VEDTAK_VEDTAKSTIDSPUNKT være ulik. Ellers er det samme
-    OMGJORT_VEDTAK_VEDTAKSTIDSPUNKT,
     INNEVÆRENDE_MÅNED,
     ETTERFØLGENDE_MANUELL_VEDTAK,
 }
