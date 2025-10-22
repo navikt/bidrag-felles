bf0b48922574979c9d84f4f1b6a91a546c8f7eb6
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2025-10-22 14:00:10 +0200

    Nye dtos for bidrag-bbm søknader (#709)

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/Søknad.kt
@@ -0,0 +1,73 @@
+package no.nav.bidrag.transport.behandling.beregning.felles
+
+import no.nav.bidrag.domene.enums.behandling.Behandlingstype
+import no.nav.bidrag.domene.enums.rolle.SøktAvType
+import no.nav.bidrag.domene.enums.vedtak.Stønadstype
+import java.math.BigDecimal
+import java.time.LocalDate
+
+data class FeilregistrerSøknadRequest(
+    val søknadsid: String,
+)
+
+data class HentBPsÅpneSøknaderRequest(
+    val personidentBP: String,
+)
+
+data class HentBPsÅpneSøknaderResponse(
+    val åpneSøknader: List<ÅpenSøknadDto> = emptyList(),
+)
+
+data class ÅpenSøknadDto(
+    val behandlingstype: Behandlingstype,
+    val saksnummer: String,
+    val søknadsid: String,
+    val stønadstype: Stønadstype,
+    val behandlingsid: String?,
+    val søknadMottattDato: LocalDate,
+    val søknadFomDato: LocalDate?,
+    val søktAvType: SøktAvType,
+    val partISøknadListe: List<PartISøknad> = emptyList(),
+)
+
+data class PartISøknad(
+    val personident: String?,
+    val rolletype: String,
+    val innbetaltBeløp: BigDecimal? = BigDecimal.ZERO,
+    val gebyr: Boolean = false,
+)
+
+data class LeggTilBarnIFFSøknadRequest(
+    val søknadsid: String,
+    val personidentBarn: String,
+    val innkreving: Boolean,
+)
+
+data class OppdaterBehandlerenhetRequest(
+    val behandlerenhet: String,
+    val søknadsid: String,
+)
+
+data class OppdaterBehandlingsidRequest(
+    val eksisterendeBehandlingsid: String? = null,
+    val nyBehandlingsid: String,
+    val søknadsid: String,
+)
+
+data class OpprettSøknadRequest(
+    val saksnummer: String,
+    val stønadstype: Stønadstype,
+    val behandlingsid: String?,
+    val enhet: String,
+    val søknadFomDato: LocalDate,
+    val barnListe: List<Barn>,
+)
+
+data class Barn(
+    val personident: String,
+    val innkreving: Boolean,
+)
+
+data class OpprettSøknaderResponse(
+    val søknadsid: String,
+)
