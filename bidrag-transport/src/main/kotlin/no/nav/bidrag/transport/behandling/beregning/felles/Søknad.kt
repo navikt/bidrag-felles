ce28b6a49bd1b4689496c238d68092836629d0bc
Magnus Rinnan <magnus.rinnan@nav.no>
2025-10-23 14:34:24 +0200

    Endret ÅpenSøknadDto

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/Søknad.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/beregning/felles/Søknad.kt
@@ -1,9 +1,11 @@
 package no.nav.bidrag.transport.behandling.beregning.felles
 
+import no.nav.bidrag.domene.enums.behandling.Behandlingstatus
 import no.nav.bidrag.domene.enums.behandling.Behandlingstype
 import no.nav.bidrag.domene.enums.rolle.Rolletype
 import no.nav.bidrag.domene.enums.rolle.SøktAvType
 import no.nav.bidrag.domene.enums.vedtak.Stønadstype
+import no.nav.bidrag.transport.behandling.hendelse.BehandlingStatusType
 import java.math.BigDecimal
 import java.time.LocalDate
 
@@ -24,6 +26,7 @@ data class ÅpenSøknadDto(
     val saksnummer: String,
     val søknadsid: String,
     val stønadstype: Stønadstype,
+    val innkreving: Boolean,
     val behandlingsid: String?,
     val søknadMottattDato: LocalDate,
     val søknadFomDato: LocalDate?,
@@ -61,14 +64,41 @@ data class OpprettSøknadRequest(
     val behandlingsid: String?,
     val enhet: String,
     val søknadFomDato: LocalDate,
+    val innkreving: Boolean,
     val barnListe: List<Barn>,
 )
 
 data class Barn(
     val personident: String,
-    val innkreving: Boolean,
 )
 
 data class OpprettSøknaderResponse(
     val søknadsid: String,
+)
+
+data class HentSøknadRequest(
+    val søknadsid: String,
+)
+
+data class HentSøknadResponse(
+    val søknad: HentSøknad,
+)
+
+data class HentSøknad(
+    val søknadsid: String,
+    val blankettid: String,
+    val søknadMottattDato: LocalDate,
+    val søknadFomDato: LocalDate? = null,
+    val stønadstype: Stønadstype,
+    val behandlerenhet: String? = null,
+    val saksnummer: String,
+    val behandlingsid: String? = null,
+    val behandlingStatusType: BehandlingStatusType,
+    val hentSøknadslinjerListe: List<HentSøknadslinje> = emptyList(),
+)
+
+data class HentSøknadslinje(
+    val søknadslinjeid: String,
+    val personidentBarn: String,
+    val behandlingstatus: Behandlingstatus,
 )
