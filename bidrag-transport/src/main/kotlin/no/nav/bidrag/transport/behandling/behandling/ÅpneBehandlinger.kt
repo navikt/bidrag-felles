47d5cfc0f13b0d2423ce9d4f52e57bd59b0821f3
Ugur Alpay Cenar <cenar93@gmail.com>
2025-10-27 08:43:06 +0100

    Åpen behandling respons

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/behandling/ÅpneBehandlinger.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/behandling/ÅpneBehandlinger.kt
@@ -1,16 +1,27 @@
 package no.nav.bidrag.transport.behandling.behandling
 
 import no.nav.bidrag.domene.enums.vedtak.Stønadstype
+import java.time.LocalDate
 
 data class HentÅpneBehandlingerRequest(
     val barnIdent: String,
 )
 
 data class HentÅpneBehandlingerRespons(
-    val åpneBehandling: List<ÅpenBehandling>,
+    val behandlinger: List<ÅpenBehandling>,
 )
 
 data class ÅpenBehandling(
     val stønadstype: Stønadstype,
     val behandlingId: Long,
+    val barn: List<ÅpenBehandlingBarn>,
+)
+
+data class ÅpenBehandlingBarn(
+    val saksnummer: String,
+    val søknadsid: Long,
+    val bidragsmottakerIdent: String,
+    val ident: String,
+    val søktFraDato: LocalDate,
+    val mottattDato: LocalDate,
 )
