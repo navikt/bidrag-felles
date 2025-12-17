e8eeec17acfd6a6b16105bdb2f3105959fc419f2
Magnus Rinnan <magnus.rinnan@nav.no>
2025-12-17 16:22:01 +0100

    Ny request manuelle vedtak

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/request/HentManuelleVedtakRequest.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/request/HentManuelleVedtakRequest.kt
@@ -1,5 +1,9 @@
 package no.nav.bidrag.transport.behandling.vedtak.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+import no.nav.bidrag.domene.ident.Personident
+import java.time.YearMonth
+
 data class HentManuelleVedtakRequest(
     @Schema(description = "PeriodeFom som vedtak skal sjekkes mot. Vedtak eldre enn denne datoen skal ikke returneres")
     val periodeFom: YearMonth,
