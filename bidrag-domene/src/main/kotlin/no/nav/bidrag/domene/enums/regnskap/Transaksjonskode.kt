17a57953ce2681f1a95e71ef2423437ae471ad55
Simen Høston <simen.naper.hoston@webstep.no>
2026-01-20 14:01:59 +0100

    Legger til en utfyllende liste over alle transaksjonskoder med beskrivelse av alle kodene

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/regnskap/Transaksjonskode.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/regnskap/Transaksjonskode.kt
@@ -6,30 +6,77 @@ import no.nav.bidrag.domene.enums.vedtak.Stønadstype
 enum class Transaksjonskode(
     val korreksjonskode: String?,
     val negativtBeløp: Boolean,
+    val beskrivelse: String,
 ) {
-    A1("A3", false), // Bidragsforskudd
-    A3(null, true),
-    B1("B3", false), // Underholdsbidrag (m/u tilleggsbidrag)
-    B3(null, true),
-    D1("D3", false), // 18årsbidrag
-    D3(null, true),
-    E1("E3", false), // Bidrag til særlige utgifter (særtilskudd)
-    E3(null, true),
-    F1("F3", false), // Ektefellebidrag
-    F3(null, true),
-    G1("G3", false), // Gebyr
-    G3(null, true),
-    H1("H3", false), // Tilbakekreving
-    H3(null, true),
-    I1(null, true), // Motregning
-    K1(null, true), // Ettergivelse
-    K2(null, true), // Direkte oppgjør (innbetalt beløp)
-    K3(null, true), // Tilbakekreving ettergivelse
-    M1("M3", false), // Tilbakekreving bidrag
-    M3(null, true),
+    A1("A3", false, "Bidragsforskudd"),
+    A2(null, false, "Forskudd korrigering"),
+    A3(null, true, "Korreksjonskode for A1"),
+    A4(null, false, "Forskudd utbetaling"),
+    A5(null, false, "Forskudd feilutbetaling"),
+    A6(null, false, "Forskudd erstatningsutbetaling"),
+    A7(null, false, "Forskudd returføring utbetalin"),
+    A10(null, false, "Midlertidig forskuddsats"),
+    B1("B3", false, "Underholdsbidrag (m/u tilleggsbidrag)"),
+    B2(null, false, "Privat bidrag korrigering"),
+    B3(null, true, "Korreksjonskode for B1"),
+    B4(null, false, "Privat oppgjør bidrag privat"),
+    B10(null, false, "Privat bidrag utbetaling"),
+    C1(null, false, "Offentlig bidrag"),
+    C2(null, false, "Offentlig bidrag korrigering"),
+    C4(null, false, "Off bidrag BP tilbakebetalt forskudd"),
+    C5(null, false, "Off bidrag tilbakeført innkrevingssak"),
+    D1("D3", false, "18årsbidrag"),
+    D2(null, false, "Bidrag privat 18 år korrigering"),
+    D3(null, true, "Korreksjonskode for D1"),
+    D4(null, false, "Privat oppgjør bidrag 18 år"),
+    D10(null, false, "Bidrag privat 18 år utbetaling"),
+    E1("E3", false, "Bidrag til særlige utgifter (særtilskudd)"),
+    E2(null, false, "Særtilskudd korrigering"),
+    E3(null, true, "Korreksjonskode for E1"),
+    E4(null, false, "Privat oppgjør særtilskudd"),
+    E10(null, false, "Særtilskudd utbetaling"),
+    F1("F3", false, "Ektefellebidrag"),
+    F2(null, false, "Ektefellebidrag korrigering"),
+    F3(null, true, "Korreksjonskode for F1"),
+    F4(null, false, "Privat oppgjør ektefellebidrag"),
+    F10(null, false, "Utbetaling ektefellebidrag"),
+    G1("G3", false, "Gebyr"),
+    G2(null, false, "Fastsettelsesgebyr korrigering"),
+    G3(null, true, "Korreksjonskode for G1"),
+    G4(null, false, "Fastsettelsesgebyr tilbakebetaling"),
+    G5(null, false, "Fastsettelsesgebyr tilb.ført innkr.sak"),
+    H1("H3", false, "Tilbakekrevd forskudd"),
+    H2(null, false, "Tilbakekrevd forskudd korrigering"),
+    H3(null, true, "Korreksjonskode for H1"),
+    H5(null, false, "Tilbakekrevd forskudd tilb.ført innkr"),
+    I1(null, false, "Motregning"),
+    I2(null, false, "Motregning korrigering"),
+    J1(null, false, "Kommunale forskuddskrav"),
+    J2(null, false, "Kommunale stønadskrav"),
+    J3(null, false, "Folketrygdens stønadskrav"),
+    J10(null, false, "Gamle kommunale krav utbetaling"),
+    K1(null, false, "Ettergivelse"),
+    K2(null, false, "Direkte oppgjør (innbetalt beløp)"),
+    K3(null, false, "Tilbakekreving ettergivelse"),
+    K10(null, false, "Ettergivelse korrigering"),
+    M1("M3", false, "Tilbakekreving bidrag"),
+    M3(null, true, "Korreksjonskode for M1"),
+    T301(null, false, "OCR innbetaling"),
+    T302(null, false, "Trygdetrekk"),
+    T303(null, false, "Trekk i utbetaling"),
+    T304(null, false, "Aetat innbetaling"),
+    T305(null, false, "Innbetaling Adra"),
+    T307(null, false, "Tilbakeført fra reskontro"),
+    T309(null, false, "Manuelt ført innbetaling"),
+    T371(null, false, "Tilbakebetaling"),
+    T390(null, false, "Annullert innbet"),
+    T400(null, false, "Avskrivning"),
+    T401(null, false, "Innbetaling/avskriving"),
     ;
 
     companion object {
+        fun hentAlle(): List<Transaksjonskode> = entries.toList()
+
         fun hentTransaksjonskodeForType(type: String): Transaksjonskode =
             when (type) {
                 Stønadstype.FORSKUDD.name -> A1
