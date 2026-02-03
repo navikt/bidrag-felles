5f673e380d33ea94bf645f5825548b7237f01f1e
LarsOttoHaugen <lars.otto.haugen@nav.no>
2026-02-03 08:31:58 +0100

    Justering på objekt SakDetaljerGrunnlag

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/SakDetaljerGrunnlag.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/SakDetaljerGrunnlag.kt
@@ -1,10 +1,11 @@
 package no.nav.bidrag.transport.behandling.felles.grunnlag
 
+import no.nav.bidrag.domene.enums.sak.Sakskategori
 import no.nav.bidrag.domene.tid.ÅrMånedsperiode
 
 data class SakDetaljerGrunnlag(
     override val periode: ÅrMånedsperiode,
     override val manueltRegistrert: Boolean = false,
     val saksnummer: String,
-    val gjelderUtland: Boolean = false,
+    val sakskategori: Sakskategori = Sakskategori.N,
 ) : GrunnlagPeriodeInnhold
