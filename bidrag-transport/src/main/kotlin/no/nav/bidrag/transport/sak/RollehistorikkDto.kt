5979fcd2fc69b234dad5a56e3a73574e30b794e9
Magnus Rinnan <44389619+rinnan17@users.noreply.github.com>
2026-02-05 16:13:33 +0100

    Lagt til RollehistorikkDto (#785)
    * Lagt til RollehistorikkDto
    
    * Lagt til RollehistorikkDto

---
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/sak/RollehistorikkDto.kt
@@ -0,0 +1,33 @@
+package no.nav.bidrag.transport.sak
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+import no.nav.bidrag.domene.enums.rolle.Rolletype
+import no.nav.bidrag.domene.ident.Personident
+import no.nav.bidrag.domene.ident.ReellMottaker
+
+data class RollehistorikkDto(
+    @field:Schema(
+        description = "Personens fødselsnummer. Påkrevd for alle",
+    )
+    @field:JsonProperty("fodselsnummer")
+    val fødselsnummer: Personident? = null,
+    @field:Schema(
+        description =
+            "Kode for rolletype tilsvarende kodene i T_KODE_ROLLETYPE. " +
+                "Gyldige verdier er f.eks. BM (bidragsmottaker), BP (bidragspliktig) og BA (barn).",
+    )
+    val type: Rolletype,
+    @field:Schema(
+        description =
+            "Reell mottaker (RM) for barnet. " +
+                "Kan kun registreres for barn (BA), og kan representere både person eller samhandler (organisasjon/verge). " +
+                "Dette feltet brukes i stedet for samhandlerIdent når bidrag skal utbetales til annen part enn barnets BM.",
+    )
+    val reellMottaker: ReellMottakerDto? = null,
+    @field:Schema(
+        description =
+            "Beskrivelse av hva slags type endring som er gjort for rolle",
+    )
+    val typeEndring: String? = null,
+)
