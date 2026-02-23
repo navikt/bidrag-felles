5cf68801427e9fc6a6bd830fa44f18abcd45df5a
LarsOttoHaugen <lars.otto.haugen@nav.no>
2026-02-23 12:13:36 +0100

    Lagt til skattefaktor i objekt Barnetillegg

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Barnetillegg.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/felles/grunnlag/Barnetillegg.kt
@@ -36,4 +36,5 @@ data class Barnetillegg(
     val barnetilleggType: Inntektstype,
     val bruttoBarnetillegg: BigDecimal,
     val nettoBarnetillegg: BigDecimal,
+    val skattefaktor: BigDecimal,
 )
