b340cbe7b54a88bf62cc9f9a35afdd21100f3a17
Magnus Rinnan <magnus.rinnan@nav.no>
2026-02-26 11:25:12 +0100

    Fjerner periodeFom i request

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/request/HentManuelleVedtakRequest.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/behandling/vedtak/request/HentManuelleVedtakRequest.kt
@@ -5,8 +5,6 @@ import no.nav.bidrag.domene.ident.Personident
 import java.time.YearMonth
 
 data class HentManuelleVedtakRequest(
-    @Schema(description = "PeriodeFom som vedtak skal sjekkes mot. Vedtak eldre enn denne datoen skal ikke returneres")
-    val periodeFom: YearMonth,
     @Schema(description = "Bidragspliktiges personident")
     val skyldner: Personident,
 )
