006e7fa92091e78ab1b85697339a76082085eaa7
Ugur Alpay Cenar <cenar93@gmail.com>
2026-03-02 07:39:54 +0100

    Ny enum for Arbeidsfordeling

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/sak/Arbeidsfordeling.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/enums/sak/Arbeidsfordeling.kt
@@ -2,6 +2,9 @@
 
 package no.nav.bidrag.domene.enums.sak
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(enumAsRef = true)
 enum class Arbeidsfordeling(
     private val beskrivelse: String,
     val behandlingstema: String?,
@@ -11,5 +14,19 @@ enum class Arbeidsfordeling(
     EFS("Ektefellesak", "ab0325"),
     FRS("Farskap", "ab0322"),
     INH("Settekontor", null),
-    OPS("Oppfostringssak", "ab0324)"),
+    OPS("Oppfostringssak", "ab0324"),
+}
+
+@Schema(enumAsRef = true)
+enum class ArbeidsfordelingV2(
+    private val bisysKode: String,
+    val behandlingstema: String?,
+) {
+    BARNEBORTFØRING("BBF", "ab0323"),
+    EIERENHET("EEN", null),
+    EKTEFELLLESAK("EFS", "ab0325"),
+    FARSKAP("FRS", "ab0322"),
+    SETTEKONTOR("INH", null),
+    OPPFOSTRINGSSAK("OPS", "ab0324"),
+    REISEKOSTNADSAK("OPS", "ab0129"),
 }
