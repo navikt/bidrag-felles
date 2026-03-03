8534c60fec302de6eae0097cdd9f5b48bfd86ea2
Ugur Alpay Cenar <cenar93@gmail.com>
2026-03-03 11:29:03 +0100

    Legg til tilJsonString metode i JsonUtils

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/felles/JsonUtils.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/felles/JsonUtils.kt
@@ -8,3 +8,5 @@ val commonObjectmapper =
         .findAndRegisterModules()
         .configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
         .configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+fun tilJsonString(value: Any): String = commonObjectmapper.writeValueAsString(value)
