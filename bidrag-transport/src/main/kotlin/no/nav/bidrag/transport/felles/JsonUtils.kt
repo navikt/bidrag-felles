6ad8cf632e1029ae7496b0ae2526996ed9958de7
Ugur Alpay Cenar <cenar93@gmail.com>
2026-03-12 13:11:53 +0100

    Oppdater JsonUtils for å håndtere ukjente enum-verdier og forbedre datakonfigurasjon

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/felles/JsonUtils.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/felles/JsonUtils.kt
@@ -1,12 +1,15 @@
 package no.nav.bidrag.transport.felles
 
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 
 // Bisys bruker eldre versjon av jackson og støtter derfor ikke JsonMapper
 val commonObjectmapper =
     ObjectMapper()
         .findAndRegisterModules()
-        .configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-        .configure(com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        .configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_USING_DEFAULT_VALUE, true)
+        .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
 fun tilJsonString(value: Any): String = commonObjectmapper.writeValueAsString(value)
