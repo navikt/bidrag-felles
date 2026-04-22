f7f541e807bae5f266e195b96b1c9b30c16c8630
Simen Høston <simen.naper.hoston@webstep.no>
2026-04-22 12:10:02 +0200

    Fjerner convertere for domeneobjekter

--- bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/sak/Saksnummer.kt
+++ bidrag-domene/src/main/kotlin/no/nav/bidrag/domene/sak/Saksnummer.kt
@@ -2,10 +2,7 @@
 
 package no.nav.bidrag.domene.sak
 
-import jakarta.persistence.AttributeConverter
 import no.nav.bidrag.domene.felles.Verdiobjekt
-import no.nav.bidrag.domene.util.trimToNull
-import org.springframework.core.convert.converter.Converter
 
 class Saksnummer(
     override val verdi: String,
@@ -15,19 +12,4 @@ class Saksnummer(
     companion object {
         private val SEVEN_DIGITS_REGEX = Regex("^\\d{7}$")
     }
-}
-
-class SaksnummerReadingConverter : Converter<String, Saksnummer?> {
-    override fun convert(source: String) = source.trimToNull()?.let { Saksnummer(source) }
-}
-
-class SaksnummerWritingConverter : Converter<Saksnummer, String?> {
-    override fun convert(source: Saksnummer) = source.verdi.trimToNull()
-}
-
-@jakarta.persistence.Converter
-class SaksnummerConverter : AttributeConverter<Saksnummer, String> {
-    override fun convertToEntityAttribute(source: String?) = source?.trimToNull()?.let { Saksnummer(source) }
-
-    override fun convertToDatabaseColumn(source: Saksnummer?) = source?.verdi.trimToNull()
 }
