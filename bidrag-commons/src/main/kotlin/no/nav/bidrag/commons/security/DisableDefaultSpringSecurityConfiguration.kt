e174dbbd6c46fff696f295fbb7163506346e8a29
Simen Høston <simen.naper.hoston@webstep.no>
2026-04-23 08:29:25 +0200

    legger til sikkerhetsfilterkjede i DisableDefaultSpringSecurityConfiguration

--- bidrag-commons/src/main/kotlin/no/nav/bidrag/commons/security/DisableDefaultSpringSecurityConfiguration.kt
+++ bidrag-commons/src/main/kotlin/no/nav/bidrag/commons/security/DisableDefaultSpringSecurityConfiguration.kt
@@ -1,9 +1,12 @@
 package no.nav.bidrag.commons.security
 
 import org.springframework.boot.security.autoconfigure.web.servlet.ConditionalOnDefaultWebSecurity
+import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
 
 @ConditionalOnDefaultWebSecurity
 @EnableWebSecurity
@@ -14,4 +17,7 @@ class DisableDefaultSpringSecurityConfiguration : WebSecurityConfigurer<WebSecur
     override fun configure(web: WebSecurity) {
         web.ignoring().requestMatchers("/**")
     }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain = http.build()
 }
