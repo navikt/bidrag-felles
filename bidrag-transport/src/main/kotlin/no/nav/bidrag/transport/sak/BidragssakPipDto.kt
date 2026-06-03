864823d3784c6cd93960a93de55eb03ced3903b1
Alexander Raknes <aleksander.raknes@nav.no>
2026-06-03 15:20:30 +0200

    fjernet paragraf 19 fra dto

--- bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/sak/BidragssakPipDto.kt
+++ bidrag-transport/src/main/kotlin/no/nav/bidrag/transport/sak/BidragssakPipDto.kt
@@ -6,13 +6,11 @@ import no.nav.bidrag.domene.sak.Saksnummer
 @Schema(
     description =
         "Metadata for pip tjeneste " +
-            "(paragraf 19 på bidragssak, samt fnr for involverte roller",
+            "(fnr for involverte roller)",
 )
 data class BidragssakPipDto(
     @Schema(description = "Saksnummeret til bidragssaken")
     val saksnummer: Saksnummer,
-    @Schema(description = "Om saken omhandler paragraf 19")
-    val erParagraf19: Boolean = false,
     @Schema(description = "Om saken er avsluttet")
     val avsluttet: Boolean = false,
     @Schema(description = "Fødselsnummer til personer innvolvert i bidragssaken")
